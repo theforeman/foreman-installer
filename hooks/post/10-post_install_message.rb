bb5944a02cb8e45f48781f170c05ca1625d7a4f6
Shlomi Zadok <shlomi@ben-hanna.com>
2014-11-12 15:48:04 +0200

    fixes #8008 - display foreman, foreman-proxy and puppet regradless to success or fail

--- hooks/post/10-post_install_message.rb
+++ hooks/post/10-post_install_message.rb
@@ -1,24 +1,24 @@
 # Puppet status codes say 0 for unchanged, 2 for changed succesfully
 if [0,2].include? @kafo.exit_code
   say "  <%= color('Success!', :good) %>"
-
-  # Foreman UI?
-  if @kafo.config.module_enabled? 'foreman'
-    say "  * <%= color('Foreman', :info) %> is running at <%= color('#{param('foreman','foreman_url').value}', :info) %>"
-    say "      Initial credentials are <%= color('#{param('foreman', 'admin_username').value}', :info) %> / <%= color('#{param('foreman', 'admin_password').value}', :info) %>" if param('foreman','authentication').value == true
-  end
-
-  # Proxy?
-  if @kafo.config.module_enabled? 'foreman_proxy'
-    say "  * <%= color('Foreman Proxy', :info) %> is running at <%= color('#{param('foreman_proxy','registered_proxy_url').value}', :info) %>"
-  end
-
-  # Puppetmaster?
-  if ( @kafo.config.module_enabled?('puppet') && ( param('puppet','server').value != false ) )
-    say "  * <%= color('Puppetmaster', :info) %> is running at <%= color('port #{param('puppet','server_port').value}', :info) %>"
-  end
 else
   say "  <%= color('Something went wrong!', :bad) %> Check the log for ERROR-level output"
+end
+
+# Foreman UI?
+if @kafo.config.module_enabled? 'foreman'
+  say "  * <%= color('Foreman', :info) %> is running at <%= color('#{param('foreman','foreman_url').value}', :info) %>"
+  say "      Initial credentials are <%= color('#{param('foreman', 'admin_username').value}', :info) %> / <%= color('#{param('foreman', 'admin_password').value}', :info) %>" if param('foreman','authentication').value == true
+end
+
+# Proxy?
+if @kafo.config.module_enabled? 'foreman_proxy'
+  say "  * <%= color('Foreman Proxy', :info) %> is running at <%= color('#{param('foreman_proxy','registered_proxy_url').value}', :info) %>"
+end
+
+# Puppetmaster?
+if ( @kafo.config.module_enabled?('puppet') && ( param('puppet','server').value != false ) )
+  say "  * <%= color('Puppetmaster', :info) %> is running at <%= color('port #{param('puppet','server_port').value}', :info) %>"
 end
 
 # This is always useful, success or fail
