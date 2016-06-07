561e8bc6b3c52f46f9eb858d4a225131ff7fa2fa
Dominic Cleal <dominic@cleal.org>
2016-06-07 13:40:27 +0100

    Fix foreman_proxy answers migration when module's disabled
    When the foreman_proxy module is disabled, the value of the answer will
    be false rather than a hash, causing a migration error:
    
        20160518125928_foreman_proxy_puppetrun_rename.rb:3:in `block
        (2 levels) in load_migrations': undefined method `has_key?' for
        false:FalseClass (NoMethodError)

--- config/foreman.migrations/20160518125928_foreman_proxy_puppetrun_rename.rb
+++ config/foreman.migrations/20160518125928_foreman_proxy_puppetrun_rename.rb
@@ -1,4 +1,4 @@
 # Rename foreman_proxy puppetrun parameters to puppet
 # https://github.com/theforeman/puppet-foreman_proxy/commit/c26cac15
-answers['foreman_proxy']['puppet'] = answers['foreman_proxy'].delete('puppetrun') if answers['foreman_proxy'].has_key?('puppetrun')
-answers['foreman_proxy']['puppet_listen_on'] = answers['foreman_proxy'].delete('puppetrun_listen_on') if answers['foreman_proxy'].has_key?('puppetrun_listen_on')
+answers['foreman_proxy']['puppet'] = answers['foreman_proxy'].delete('puppetrun') if answers['foreman_proxy'] && answers['foreman_proxy'].has_key?('puppetrun')
+answers['foreman_proxy']['puppet_listen_on'] = answers['foreman_proxy'].delete('puppetrun_listen_on') if answers['foreman_proxy'] && answers['foreman_proxy'].has_key?('puppetrun_listen_on')
