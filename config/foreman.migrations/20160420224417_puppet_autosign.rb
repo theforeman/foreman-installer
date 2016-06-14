00e04cdebed3eb1a8970b4dd7c5e016bd1d6c6b0
Dominic Cleal <dominic@cleal.org>
2016-06-14 10:24:01 +0100

    fixes #15399 - check module is enabled in answers before migrating values

--- config/foreman.migrations/20160420224417_puppet_autosign.rb
+++ config/foreman.migrations/20160420224417_puppet_autosign.rb
@@ -1,5 +1,7 @@
 # Redetermine the value of autosign, as it changed from string/boolean to path/boolean
 # in puppet-puppet a2325f1 and was deleted from puppet-foreman_proxy 9f3c9aa
-current_autosign = answers['puppet']['autosign']
-answers['puppet'].delete('autosign') unless !!current_autosign == current_autosign
-answers['foreman_proxy'].delete('autosign_location')
+if answers['puppet']
+  current_autosign = answers['puppet']['autosign']
+  answers['puppet'].delete('autosign') unless !!current_autosign == current_autosign
+end
+answers['foreman_proxy'].delete('autosign_location') if answers['foreman_proxy']
