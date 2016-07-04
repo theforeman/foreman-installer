ef9621a89cb965d38a50a326430c84060852bc34
Michael Moll <kvedulv@kvedulv.de>
2016-07-04 17:25:33 +0200

    Revert "Remove unmaintained ovirt_provision plugin"
    This reverts commit 3e80656d245cfd8e05fd10021189d450333a0dc6.

--- config/foreman.migrations/20160516143923_remove_ovirt_provision.rb
+++
@@ -1,3 +0,0 @@
-# Remove unmaintained ovirt_provision plugin
-answers.delete('foreman::plugin::ovirt_provision')
-scenario[:mapping].delete(:'foreman::plugin::ovirt_provision')
---
+++ config/foreman.migrations/20160705103630_readd_ovirt_provision.rb
@@ -0,0 +1,2 @@
+answers['foreman::plugin::ovirt_provision'] ||= false
+scenario[:mapping][:'foreman::plugin::ovirt_provision'] ||= {:dir_name => 'foreman', :manifest_name => 'plugin/ovirt_provision', :params_name => 'plugin/ovirt_provision/params'}
