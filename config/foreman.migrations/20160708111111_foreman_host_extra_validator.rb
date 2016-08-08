ad62114d01fac3354871b4e04c7010b19db52e6f
Timo Goebel <timo.goebel@dm.de>
2016-08-08 11:24:33 +0200

    add plugin foreman_host_extra_validator

---
+++ config/foreman.migrations/20160708111111_foreman_host_extra_validator.rb
@@ -0,0 +1,2 @@
+answers['foreman::plugin::host_extra_validator'] ||= false
+scenario[:mapping][:'foreman::plugin::host_extra_validator'] ||= {:dir_name => 'foreman', :manifest_name => 'plugin/host_extra_validator'}
