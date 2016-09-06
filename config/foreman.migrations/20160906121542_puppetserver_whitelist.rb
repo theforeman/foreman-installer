7a82f00b9ede64d150032070a32a1dc779d736ca
Michael Moll <kvedulv@kvedulv.de>
2016-09-07 00:21:03 +0200

    handle API whitelist changes in puppet-puppet
    Closes GH-198

---
+++ config/foreman.migrations/20160906121542_puppetserver_whitelist.rb
@@ -0,0 +1,6 @@
+# Redetermine the value of API whitelists, as it changed
+# in puppet-puppet f9b4e87cd855d7d5d0bbf3a1831b5daf22cdb413
+if answers['puppet']
+  answers['puppet'].delete('server_admin_api_whitelist')
+  answers['puppet'].delete('server_ca_client_whitelist')
+end
