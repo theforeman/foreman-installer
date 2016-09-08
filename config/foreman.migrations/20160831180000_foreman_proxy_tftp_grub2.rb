2a74f60e04bf59db1775ca82a79849869f01b5c8
Lukáš Zapletal <lzap@redhat.com>
2016-09-08 17:46:05 +0200

    Fixed doms comments

--- config/foreman.migrations/20160831180000_foreman_proxy_tftp_grub2.rb
+++ config/foreman.migrations/20160831180000_foreman_proxy_tftp_grub2.rb
@@ -1,5 +1,9 @@
-root = answers['foreman_proxy']['tftp_root']
-dirs = answers['foreman_proxy']['tftp_dirs']
-dirs << "#{root}/grub"
-dirs << "#{root}/grub2"
-dirs.uniq!
+if answers['foreman_proxy']
+  root = answers['foreman_proxy']['tftp_root']
+  if answers['foreman_proxy']['tftp_dirs']
+    dirs = answers['foreman_proxy']['tftp_dirs']
+    dirs << "#{root}/grub"
+    dirs << "#{root}/grub2"
+    dirs.uniq!
+  end
+end
