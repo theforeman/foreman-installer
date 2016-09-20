bbd8401e8b8f70bb43df2bd20fa405ea746a107e
Ivan Necas <inecas@redhat.com>
2016-09-20 12:56:11 +0200

    Add migration for smart_proxy_ansible plugin

---
+++ config/foreman.migrations/20160919173201_smart_proxy_ansible.rb
@@ -0,0 +1,2 @@
+answers['foreman_proxy::plugin::ansible'] ||= false
+scenario[:mapping][:'foreman_proxy::plugin::ansible'] ||= {:dir_name => 'foreman_proxy', :manifest_name => 'plugin/ansible', :params_name => 'plugin/ansible/params'}
