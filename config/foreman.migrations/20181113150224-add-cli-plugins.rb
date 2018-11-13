c8a49a78182b08cf9a6695e6cf9afb77cff3c2a1
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2018-11-13 15:05:29 +0100

    Fixes #25445 - Add CLI plugins migration
    1d4758c43c37524b9871b1cb111fc48394aed3ce added these plugins but was
    missing a migration for upgrading users.

---
+++ config/foreman.migrations/20181113150224-add-cli-plugins.rb
@@ -0,0 +1,4 @@
+answers['foreman::cli::discovery'] ||= false
+answers['foreman::cli::remote_execution'] ||= false
+answers['foreman::cli::tasks'] ||= false
+answers['foreman::cli::templates'] ||= false
