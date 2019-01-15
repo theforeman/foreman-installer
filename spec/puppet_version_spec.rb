3bd5f42442536b07397448bba37d95eb4d646bde
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2019-01-15 16:49:11 +0100

    Add a puppet version check
    This means we execute the tests twice, but that's not a lot of overhead.
    It does mean we can run our cheap tests before doing the expensive full
    build.

---
+++ spec/puppet_version_spec.rb
@@ -0,0 +1,25 @@
+require 'json'
+require 'semantic_puppet'
+
+describe 'Puppet module' do
+  VERSIONS = ['5.5.8', '6.0.0'].map { |v| SemanticPuppet::Version.parse(v) }
+
+  Dir.glob(File.join(__dir__, '../_build/modules/*/metadata.json')).each do |filename|
+    context File.basename(File.dirname(filename)) do
+      let(:metadata) { JSON.parse(File.read(filename)) }
+      let(:requirements) { metadata['requirements'] || [] }
+      let(:puppet_requirement) do
+        requirement = requirements.find { |req| req['name'] == 'puppet' }
+        version_requirement = requirement['version_requirement'] if requirement
+        version_requirement ||= '>= 0'
+        SemanticPuppet::VersionRange.parse(version_requirement)
+      end
+
+      VERSIONS.each do |puppet_version|
+        it "should support Puppet #{puppet_version}" do
+          expect(puppet_requirement.include?(puppet_version)).to be true
+        end
+      end
+    end
+  end
+end
