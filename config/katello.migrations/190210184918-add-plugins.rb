918083b3db3f4adfea9aaf4eb1856ecf5ce91475
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2019-02-10 18:50:15 +0100

    Expose more plugins in the Katello scenario

---
+++ config/katello.migrations/190210184918-add-plugins.rb
@@ -0,0 +1,4 @@
+answers['foreman::plugin::digitalocean'] ||= false
+answers['foreman::plugin::memcache'] ||= false
+answers['foreman::plugin::monitoring'] ||= false
+answers['foreman_proxy::plugin::monitoring'] ||= false
