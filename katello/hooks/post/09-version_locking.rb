6b1bf0892695f50be0f126775aed3216ddbfccfd
Martin Bačovský <martin.bacovsky@gmail.com>
2019-04-05 10:36:23 +0200

    Refs #26409 - installation renamed to packages

--- katello/hooks/post/09-version_locking.rb
+++ katello/hooks/post/09-version_locking.rb
@@ -1,2 +1,2 @@
 lock_packages = get_custom_config(:lock_package_versions)
-Kafo::Helpers.execute('foreman-maintain installation lock --assumeyes') if lock_packages
+Kafo::Helpers.execute('foreman-maintain packages lock --assumeyes') if lock_packages
