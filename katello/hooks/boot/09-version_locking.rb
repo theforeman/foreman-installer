2fc3450eb74f570ee90d4ddc5515b5c714dda3be
Martin Bačovský <martin.bacovsky@gmail.com>
2019-04-08 13:43:13 +0200

    Refs #26409 - Readability improvements

--- katello/hooks/boot/09-version_locking.rb
+++ katello/hooks/boot/09-version_locking.rb
@@ -1,9 +1,10 @@
-return nil unless system('command -v foreman-maintain > /dev/null')
-app_option(
-  '--[no-]lock-package-versions',
-  :flag,
-  "Let installer lock versions of the installed packages to prevent\n" \
-    "unexpected breakages during system updates. The choice is remembered\n" \
-    "and used in next installer runs.",
-  :default => nil
-)
+if system('command -v foreman-maintain > /dev/null')
+  app_option(
+    '--[no-]lock-package-versions',
+    :flag,
+    "Let installer lock versions of the installed packages to prevent\n" \
+      "unexpected breakages during system updates. The choice is remembered\n" \
+      "and used in next installer runs.",
+    :default => nil
+  )
+end
