126419a94b84c2ad668f69e4407a603befe462c2
Jonathon Turel <jturel@gmail.com>
2019-07-28 14:21:16 -0400

    Fixes #27424 - Ensure DB is seeded

--- katello/hooks/post/30-upgrade.rb
+++ katello/hooks/post/30-upgrade.rb
@@ -1,5 +1,10 @@
 require 'fileutils'
 
+def db_seed
+  status = Kafo::Helpers.execute('foreman-rake db:seed')
+  fail_and_exit "Upgrade failed while seeding the DB" unless status
+end
+
 def upgrade_tasks
   status = Kafo::Helpers.execute('foreman-rake upgrade:run')
   fail_and_exit "Application Upgrade Failed" unless status
@@ -12,7 +17,11 @@ end
 
 if app_value(:upgrade)
   if [0, 2].include?(@kafo.exit_code)
-    upgrade_tasks if module_enabled?('foreman')
+    if module_enabled?('foreman')
+      db_seed
+      upgrade_tasks
+    end
+
     Kafo::Helpers.log_and_say :info, 'Upgrade completed!'
   else
     Kafo::Helpers.log_and_say :error, 'Upgrade failed during the installation phase. Fix the error and re-run the upgrade.'
