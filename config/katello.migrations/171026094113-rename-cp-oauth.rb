b3f2c7f01f9cf187db281b41171b60ce1794a247
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2019-09-04 18:28:59 +0200

    Refs #27785 - Handle katello not being a hash in migrations

--- config/katello.migrations/171026094113-rename-cp-oauth.rb
+++ config/katello.migrations/171026094113-rename-cp-oauth.rb
@@ -1,2 +1,4 @@
-answers['katello'].delete('oauth_key') if answers['katello'].key?('oauth_key')
-answers['katello'].delete('oauth_secret') if answers['katello'].key?('oauth_secret')
+if answers['katello'].is_a?(Hash)
+  answers['katello'].delete('oauth_key') if answers['katello'].key?('oauth_key')
+  answers['katello'].delete('oauth_secret') if answers['katello'].key?('oauth_secret')
+end
