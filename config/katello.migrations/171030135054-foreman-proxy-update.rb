05ad2405e8683767f786e5591ea1d1c9ec5c8b6f
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2019-09-12 19:10:11 +0200

    Fixes #27848 - Disable HTTP by default on Katello's proxy
    The HTTP and templates features are useful when a host needs to be
    provisioned and can't reach the Foreman server directly. Since this is
    by definition the Foreman server, the client should be able to reach it.
    
    The values are dropped since they match the module defaults. It doesn't
    implement a migration to not touch existing installations which might
    have started to rely on it.
    
    This reverts #19720.

--- config/katello.migrations/171030135054-foreman-proxy-update.rb
+++
@@ -1,4 +0,0 @@
-# foreman_proxy defaults for templates
-
-answers['foreman_proxy']['http'] = true if answers['foreman_proxy']
-answers['foreman_proxy']['templates'] = true if answers['foreman_proxy']
