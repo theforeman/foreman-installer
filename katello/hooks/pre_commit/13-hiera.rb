cb7e0c91830d6c4b70de8f598b4c061ccef5e44c
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2019-11-22 13:41:20 +0100

    Refs #27775 - avoid built in keyword new
    (cherry picked from commit e453c01e47d644b87ef4e71fc73ae3e91f25b84b)

--- katello/hooks/pre_commit/13-hiera.rb
+++ katello/hooks/pre_commit/13-hiera.rb
@@ -1,17 +1,17 @@
 TUNING_SIZES = ['default'] + ['medium', 'large', 'extra-large', 'extra-extra-large']
 TUNING_FACT = 'tuning'
 
-current = get_custom_fact(TUNING_FACT)
-new = app_value(:tuning)
+current_tuning = get_custom_fact(TUNING_FACT)
+new_tuning = app_value(:tuning)
 
-if current != new
-  unless TUNING_SIZES.include?(new)
+if current_tuning != new_tuning
+  unless TUNING_SIZES.include?(new_tuning)
     say "<%= color('Invalid tuning profile', :bad) %>"
-    say "'#{new}' is not one of #{TUNING_SIZES.join(', ')}"
+    say "'#{new_tuning}' is not one of #{TUNING_SIZES.join(', ')}"
     exit 101
   end
 
-  store_custom_fact(TUNING_FACT, new)
+  store_custom_fact(TUNING_FACT, new_tuning)
   # Store the app config to disk
   kafo.config.configure_application
 end
