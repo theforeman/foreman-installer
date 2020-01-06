45d3bb0970bb43c9307847f5e1793f17ac2d1b59
Eric D. Helms <ericdhelms@gmail.com>
2020-01-06 13:03:32 -0500

    Fixes #28653: Introduce install_package command
    This also combines all Kafo extension functions into a single hook.

--- katello/hooks/pre/29-install_postgresql.rb
+++ katello/hooks/pre/29-install_postgresql.rb
@@ -1,10 +1,4 @@
 # Working around https://tickets.puppetlabs.com/browse/PUP-2169
 if local_postgresql? && facts[:os][:release][:major] == '7' && !app_value(:upgrade)
-  package = 'rh-postgresql10-postgresql-server'
-
-  `rpm -q #{package}`
-
-  unless $?.success?
-    execute("yum -y install #{package}")
-  end
+  ensure_package('rh-postgresql10-postgresql-server', 'installed')
 end
