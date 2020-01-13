6187d8eb4d206d4632eaef92bc6f0634844bd7ce
Eric D. Helms <ericdhelms@gmail.com>
2020-01-13 12:39:10 -0500

    Refs #28530: Ensure hook dir is set using proper installer dir

--- config/katello.migrations/191218160614-update-hook-dirs.rb
+++ config/katello.migrations/191218160614-update-hook-dirs.rb
@@ -1,4 +1,4 @@
 if File.basename(scenario[:installer_dir]) == 'katello'
-  scenario[:hook_dirs] = [File.join(scenario[:installer_dir], 'katello/hooks')]
   scenario[:installer_dir] = File.dirname(scenario[:installer_dir])
+  scenario[:hook_dirs] = [File.join(scenario[:installer_dir], 'katello/hooks')]
 end
