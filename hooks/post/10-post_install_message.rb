7c0d4b11a731a881f5b0aa4332324388e21295d1
Eric D. Helms <ericdhelms@gmail.com>
2020-01-13 08:52:12 -0500

    Fixes #28717: Don't repeat success or show success when failed

--- hooks/post/10-post_install_message.rb
+++ hooks/post/10-post_install_message.rb
@@ -1,28 +1,26 @@
 # Puppet status codes say 0 for unchanged, 2 for changed succesfully
 if [0,2].include? @kafo.exit_code
-  Kafo::MessageHelpers.success_message
+  # Foreman UI?
+  if module_enabled? 'foreman'
+    Kafo::MessageHelpers.server_success_message(@kafo)
+    Kafo::MessageHelpers.new_install_message(@kafo) if new_install?
+  end
+
+  if module_enabled?('katello')
+    Kafo::MessageHelpers.certs_generate_command_message
+  end
+
+  if module_enabled?('katello_devel')
+    Kafo::MessageHelpers.dev_server_success_message(@kafo)
+    Kafo::MessageHelpers.dev_new_install_message(@kafo) if new_install?
+  end
+
+  # Proxy?
+  if module_enabled? 'foreman_proxy'
+    Kafo::MessageHelpers.proxy_success_message(@kafo)
+  end
 else
   Kafo::MessageHelpers.failure_message
-end
-
-# Foreman UI?
-if module_enabled? 'foreman'
-  Kafo::MessageHelpers.server_success_message(@kafo)
-  Kafo::MessageHelpers.new_install_message(@kafo) if new_install?
-end
-
-if module_enabled?('katello')
-  Kafo::MessageHelpers.certs_generate_command_message
-end
-
-if module_enabled?('katello_devel')
-  Kafo::MessageHelpers.dev_server_success_message(@kafo)
-  Kafo::MessageHelpers.dev_new_install_message(@kafo) if new_install?
-end
-
-# Proxy?
-if module_enabled? 'foreman_proxy'
-  Kafo::MessageHelpers.proxy_success_message(@kafo)
 end
 
 Kafo::MessageHelpers.log_message(@kafo)
