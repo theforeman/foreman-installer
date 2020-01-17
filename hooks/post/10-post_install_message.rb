4819014042b25057d171c8b029ff43a2dd17663f
Eric D. Helms <ericdhelms@gmail.com>
2020-01-16 21:29:03 -0500

    Refs #28549: Turn message helpers into hook extensions

--- hooks/post/10-post_install_message.rb
+++ hooks/post/10-post_install_message.rb
@@ -2,25 +2,25 @@
 if [0, 2].include? @kafo.exit_code
   # Foreman UI?
   if module_enabled? 'foreman'
-    Kafo::MessageHelpers.server_success_message(@kafo)
-    Kafo::MessageHelpers.new_install_message(@kafo) if new_install?
+    server_success_message(@kafo)
+    new_install_message(@kafo) if new_install?
   end
 
   if module_enabled?('katello')
-    Kafo::MessageHelpers.certs_generate_command_message
+    certs_generate_command_message
   end
 
   if module_enabled?('katello_devel')
-    Kafo::MessageHelpers.dev_server_success_message(@kafo)
-    Kafo::MessageHelpers.dev_new_install_message(@kafo) if new_install?
+    dev_server_success_message(@kafo)
+    dev_new_install_message(@kafo) if new_install?
   end
 
   # Proxy?
   if module_enabled? 'foreman_proxy'
-    Kafo::MessageHelpers.proxy_success_message(@kafo)
+    proxy_success_message(@kafo)
   end
 else
-  Kafo::MessageHelpers.failure_message
+  failure_message
 end
 
-Kafo::MessageHelpers.log_message(@kafo)
+log_message(@kafo)
