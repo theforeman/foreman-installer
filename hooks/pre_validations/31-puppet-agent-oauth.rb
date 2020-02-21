44591b97fe11bcb8c55af88309166aab0fa4ac2c
Eric D. Helms <ericdhelms@gmail.com>
2020-02-21 08:54:09 -0500

    Fixes #29097: Move puppet-agent-oauth reinstall to Foreman hooks

---
+++ hooks/pre_validations/31-puppet-agent-oauth.rb
@@ -0,0 +1,7 @@
+unless app_value(:noop)
+  if File.exist?('/opt/puppetlabs/puppet/bin/ruby') && execute_command("rpm -q puppet-agent-oauth", false, false)
+    unless execute_command("/opt/puppetlabs/puppet/bin/ruby -e \"require 'oauth'\"", false, false)
+      execute("yum -y reinstall puppet-agent-oauth", false, true)
+    end
+  end
+end
