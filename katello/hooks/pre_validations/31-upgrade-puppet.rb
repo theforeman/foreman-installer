bdb23ab4c898dc81007fafb08ec63b28a3b29d28
Eric D. Helms <ericdhelms@gmail.com>
2020-02-21 08:54:09 -0500

    Fixes #29097: Move puppet-agent-oauth reinstall to Foreman hooks

--- katello/hooks/pre_validations/31-upgrade-puppet.rb
+++
@@ -1,7 +0,0 @@
-unless app_value(:noop)
-  if File.exist?('/opt/puppetlabs/puppet/bin/ruby') && system("rpm -q puppet-agent-oauth")
-    unless system("/opt/puppetlabs/puppet/bin/ruby -e \"require 'oauth'\"")
-      execute("yum -y reinstall puppet-agent-oauth")
-    end
-  end
-end
