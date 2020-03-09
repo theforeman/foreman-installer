4a4acd92c17c795caa88e247fb9ba62dc8005fac
Eric D. Helms <ericdhelms@gmail.com>
2020-03-09 09:41:52 -0400

    Refs #29052: Allow --upgrade to pass through

--- katello/hooks/pre_validations/29-upgrade.rb
+++ katello/hooks/pre_validations/29-upgrade.rb
@@ -1,3 +1,3 @@
 if app_value(:upgrade)
-  fail_and_exit 'The --upgrade option has been removed. The installer will ensure the system is in the right state. The use of --upgrade is no longer needed.'
+  log_and_say(:warning, 'The --upgrade option has been removed. The installer will ensure the system is in the right state. The use of --upgrade is no longer needed.')
 end
