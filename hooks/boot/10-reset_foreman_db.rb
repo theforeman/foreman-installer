7ca7d88d42ec5a340c3af256096fdede35a3ebe9
William Bradford Clark <wclark@redhat.com>
2020-05-27 13:13:19 -0400

    Fixes #29797 - Merge --reset-foreman-db and --reset into --reset-data

--- hooks/boot/10-reset_foreman_db.rb
+++
@@ -1,7 +0,0 @@
-app_option(
-  '--reset-foreman-db', :flag,
-  "Drop foreman database first? You will lose all data! Unfortunately we\n" +
-    "can't detect a failure at the moment so you should verify the success\n" +
-    "manually. e.g. dropping can fail when DB is currently in use.",
-  :default => false
-)
