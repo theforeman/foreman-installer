74ee56f75e940a26864d6b09ab57ac44c5bba239
Eric D. Helms <ericdhelms@gmail.com>
2020-06-16 12:04:09 -0400

    Fixes #30136: Do not proxy isos or yum on foreman proxy content

---
+++ config/foreman-proxy-content.migrations/200616155948-disable-pulp-3-proxying.rb
@@ -0,0 +1,16 @@
+FOREMAN_PROXY_CONTENT = 'foreman_proxy_content'.freeze
+PROXY_YUM = 'proxy_pulp_yum_to_pulpcore'.freeze
+PROXY_ISOS = 'proxy_pulp_isos_to_pulpcore'.freeze
+
+def upgrade
+  answers[FOREMAN_PROXY_CONTENT] = {} unless answers[FOREMAN_PROXY_CONTENT].is_a?(Hash)
+  answers[FOREMAN_PROXY_CONTENT][PROXY_YUM] = false
+  answers[FOREMAN_PROXY_CONTENT][PROXY_ISOS] = false
+end
+
+if answers[FOREMAN_PROXY_CONTENT] == true ||
+   (answers[FOREMAN_PROXY_CONTENT].is_a?(Hash) &&
+   answers[FOREMAN_PROXY_CONTENT][PROXY_YUM].nil? &&
+   answers[FOREMAN_PROXY_CONTENT][PROXY_ISOS].nil?)
+  upgrade
+end
