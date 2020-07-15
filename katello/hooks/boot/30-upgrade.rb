3c60c745fca4a98cf6e04ebbb3d7b8462373e75a
William Bradford Clark <wclark@redhat.com>
2020-07-15 14:02:44 -0400

    Fixes #29296 - remove --upgrade

--- katello/hooks/boot/30-upgrade.rb
+++
@@ -1,6 +0,0 @@
-app_option(
-  '--upgrade',
-  :flag,
-  "Deprecated. The installer will ensure the right state in all cases now. This flag will be removed in a future release.",
-  :default => false
-)
--- katello/hooks/pre_validations/29-upgrade.rb
+++
@@ -1,3 +0,0 @@
-if app_value(:upgrade)
-  log_and_say(:warn, 'The --upgrade option has been removed. The installer will ensure the system is in the right state. The use of --upgrade is no longer needed.')
-end
