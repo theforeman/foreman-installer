a62de1d4a93e46d8298baa7656543092623b464b
Eric D. Helms <ericdhelms@gmail.com>
2020-07-28 09:57:21 -0400

    Fixes #30506: Ensure selinux packages are installed before puppet run
    (cherry picked from commit 115a55787b7b5077ee4364d5e2cc23429f82bf1b)

--- hooks/pre/31_install_scl_packages.rb
+++
@@ -1,8 +0,0 @@
-# Working around https://tickets.puppetlabs.com/browse/PUP-2169
-if el7?
-  packages = []
-  packages << 'rh-postgresql12-postgresql-server' if local_postgresql?
-  packages << 'rh-redis5-redis' if local_redis?
-  packages << 'pulpcore-selinux' if pulpcore_enabled?
-  ensure_packages(packages, 'installed')
-end
