6b84bab507d4438c0be979d04e466c02df2d36ef
Eric D. Helms <ericdhelms@gmail.com>
2020-08-27 18:44:54 -0400

    Refs #29801: Fix broken mongo cache size setting

--- hooks/pre_commit/14-mongo-cache-size.rb
+++ hooks/pre_commit/14-mongo-cache-size.rb
@@ -4,12 +4,12 @@
 if pulp_enabled?
   MONGO_CACHE_SIZE_FACT = 'mongo_cache_size'.freeze
 
-  if (current_mongo_cache_size = get_custom_fact(MONGO_CACHE_SIZE_FACT))
-    required_mongo_cache_size = (facts[:memory][:system][:total_bytes] * 0.2 / 1024 / 1024 / 1024).round(2)
-    if current_mongo_cache_size != required_mongo_cache_size
-      store_custom_fact(MONGO_CACHE_SIZE_FACT, required_mongo_cache_size)
-      # Store the app config to disk
-      kafo.config.configure_application
-    end
+  current_mongo_cache_size = get_custom_fact(MONGO_CACHE_SIZE_FACT)
+  required_mongo_cache_size = (facts[:memory][:system][:total_bytes] * 0.2 / 1024 / 1024 / 1024).round(2)
+
+  if current_mongo_cache_size != required_mongo_cache_size
+    store_custom_fact(MONGO_CACHE_SIZE_FACT, required_mongo_cache_size)
+    # Store the app config to disk
+    kafo.config.configure_application
   end
 end
