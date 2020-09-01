theforeman/foreman-installer

commit 7ca7d88d42ec5a340c3af256096fdede35a3ebe9

Fixes #29797 - Merge --reset-foreman-db and --reset into --reset-data

hooks/pre/10-reset_data.rb
@@ -1,0 +1,144 @@
+# See bottom of the script for the command that kicks off the script
+require 'English'
+
+def reset
+  stop_services
+  start_services(['postgresql']) if local_postgresql?
+  empty_db_in_postgresql('foreman') if foreman_server?
+  reset_candlepin if module_enabled?('katello')
+  reset_pulp if pulp_enabled?
+  empty_db_in_postgresql('pulpcore') if pulpcore_enabled?
+end
+
+def load_db_config(db)
+  case db
+  when 'foreman'
+    module_name = 'foreman'
+    user_param = 'username'
+    db_param = 'database'
+    param_prefix = 'db_'
+  when 'candlepin'
+    module_name = 'katello'
+    user_param = 'user'
+    db_param = 'name'
+    param_prefix = 'candlepin_db_'
+  when 'pulpcore'
+    module_name = 'foreman_proxy_content'
+    user_param = 'user'
+    db_param = 'db_name'
+    param_prefix = 'pulpcore_postgresql_'
+  else
+    raise "installer module unknown for db: #{db}"
+  end
+
+  {
+    host: param_value(module_name, "#{param_prefix}host") || 'localhost',
+    port: param_value(module_name, "#{param_prefix}port") || 5432,
+    database: param_value(module_name, "#{param_prefix}#{db_param}") || db,
+    username: param_value(module_name, "#{param_prefix}#{user_param}"),
+    password: param_value(module_name, "#{param_prefix}password"),
+  }
+end
+
+def empty_db_in_postgresql(db)
+  logger.info "Dropping #{db} database!"
+
+  config = load_db_config(db)
+  if remote_host?(config[:host])
+    empty_database!(config)
+  else
+    execute("sudo -u postgres dropdb #{config[:database]}")
+  end
+end
+
+def reset_candlepin
+  execute('rm -f /var/lib/candlepin/{cpdb_done,cpinit_done}')
+  empty_db_in_postgresql(db)
+end
+
+def empty_mongo
+  logger.info "Dropping Pulp database!"
+
+  mongo_config = load_mongo_config
+  if remote_host?(mongo_config[:host])
+    empty_remote_mongo(mongo_config)
+  else
+    execute(
+      [
+        'systemctl start rh-mongodb34-mongod',
+        "mongo #{mongo_config[:database]} --eval 'db.dropDatabase();'",
+      ]
+    )
+  end
+end
+
+def load_mongo_config
+  config = {}
+  seeds = param_value('katello', 'pulp_db_seeds')
+  seed = seeds.split(',').first
+  host, port = seed.split(':') if seed
+  config[:host] = host || 'localhost'
+  config[:port] = port || '27017'
+  config[:database] = param_value('katello', 'pulp_db_name') || 'pulp_database'
+  config[:username] = param_value('katello', 'pulp_db_username')
+  config[:password] = param_value('katello', 'pulp_db_password')
+  config[:ssl] = param_value('katello', 'pulp_db_ssl') || false
+  config[:ca_path] = param_value('katello', 'pulp_db_ca_path')
+  config[:ssl_certfile] = param_value('katello', 'pulp_db_ssl_certfile')
+  config
+end
+
+def empty_remote_mongo(config)
+  if config[:ssl]
+    ssl = "--ssl"
+    if config[:ca_path]
+      ca_cert = "--sslCAFile #{config[:ca_path]}"
+      client_cert = "--sslPEMKeyFile #{config[:ssl_certfile]}" if config[:ssl_certfile]
+    end
+  end
+  username = "-u #{config[:username]}" if config[:username]
+  password = "-p #{config[:password]}" if config[:password]
+  host = "--host #{config[:host]} --port #{config[:port]}"
+  cmd = "mongo #{config[:database]} #{username} #{password} #{host} #{ssl} #{ca_cert} #{client_cert} --eval 'db.dropDatabase();'"
+  execute(cmd)
+end
+
+def reset_pulp
+  execute('rm -f /var/lib/pulp/init.flag')
+  empty_mongo
+  execute('rm -rf /var/lib/pulp/{distributions,published,repos}/*')
+end
+
+def remote_host?(hostname)
+  !['localhost', '127.0.0.1', `hostname`.strip].include?(hostname)
+end
+
+def pg_command_base(config, command, args)
+  port = "-p #{config[:port]}" if config[:port]
+  "PGPASSWORD='#{config[:password]}' #{command} -U #{config[:username]} -h #{config[:host]} #{port} #{args}"
+end
+
+def pg_sql_statement(config, statement)
+  pg_command_base(config, 'psql', "-d #{config[:database]} -t -c \"" + statement + '"')
+end
+
+# WARNING: deletes all the data from a database. No warnings. No confirmations.
+def empty_database!(config)
+  generate_delete_statements = pg_sql_statement(config, %q(
+        select string_agg('drop table if exists \"' || tablename || '\" cascade;', '')
+        from pg_tables
+        where schemaname = 'public';
+      ))
+  delete_statements = `#{generate_delete_statements}`
+  execute(pg_sql_statement(config, delete_statements)) if delete_statements
+end
+
+if app_value(:reset_data) && !app_value(:noop)
+  response = ask('Are you sure you want to continue? This will drop the databases, reset all configurations that you have made and bring all application data back to a fresh install. [y/n]')
+  if response.downcase != 'y'
+    $stderr.puts '** cancelled **'
+    exit(1)
+  else
+    reset
+  end
+end

hooks/boot/10-reset_data.rb
@@ -1,8 +1,7 @@
-# Add reset option
 app_option(
-  '--reset',
+  '--reset-data',
   :flag,
-  'This option will drop the Katello database and clear all subsequent backend data stores. ' +
+  'This option will drop all databases for Foreman and subsequent backend systems. ' +
   "You will lose all data!\nUnfortunately, we " +
   "can't detect a failure, so you should verify success " +
   "manually.\nDropping can fail when the DB is in use.",

hooks/boot/01-kafo-hook-extensions.rb
@@ -65,6 +65,10 @@ module HookContextExtension
     (foreman_server? && !param_value('foreman', 'jobs_sidekiq_redis_url')) || pulpcore_enabled? || devel_scenario?
   end
 
+  def pulp_enabled?
+    module_enabled?('foreman_proxy_plugin_pulp') && (param_value('foreman_proxy_plugin_pulp', 'pulpnode_enabled') || param_value('foreman_proxy_plugin_pulp', 'enabled'))
+  end
+
   def pulpcore_enabled?
     module_enabled?('foreman_proxy_plugin_pulp') && param_value('foreman_proxy_plugin_pulp', 'pulpcore_enabled')
   end

hooks/boot/10-reset_foreman_db.rb
@@ -1,7 +1,0 @@
-app_option(
-  '--reset-foreman-db', :flag,
-  "Drop foreman database first? You will lose all data! Unfortunately we\n" +
-    "can't detect a failure at the moment so you should verify the success\n" +
-    "manually. e.g. dropping can fail when DB is currently in use.",
-  :default => false
-)

hooks/pre/10-reset_foreman_db.rb
@@ -1,18 +1,0 @@
-# See bottom of the script for the command that kicks off the script
-require 'English'
-
-def reset_foreman_db
-  `which foreman-rake > /dev/null 2>&1`
-  if $CHILD_STATUS.success?
-    logger.info 'Dropping database!'
-    output = `foreman-rake db:drop 2>&1`
-    logger.debug output.to_s
-    unless $CHILD_STATUS.success?
-      logger.warn "Unable to drop DB, ignoring since it's not fatal, output was: '#{output}''"
-    end
-  else
-    logger.warn 'Foreman not installed yet, can not drop database!'
-  end
-end
-
-reset_foreman_db if app_value(:reset_foreman_db) && !app_value(:noop)

katello/hooks/pre/10-reset_feature.rb
@@ -1,187 +1,0 @@
-# See bottom of the script for the command that kicks off the script
-require 'English'
-
-def reset
-  if foreman_installed?
-
-    stop_services
-    start_services(['postgresql'])
-    reset_database
-    reset_candlepin
-    reset_pulp
-    reset_pulpcore if pulpcore_enabled?
-
-  else
-    Kafo::KafoConfigure.logger.warn 'Katello not installed yet, can not drop database!'
-  end
-end
-
-def foreman_installed?
-  `which foreman-rake > /dev/null 2>&1`
-  $CHILD_STATUS.success?
-end
-
-def load_foreman_config
-  db_config = {}
-  db_config[:host] = param_value('foreman', 'db_host') || 'localhost'
-  db_config[:port] = param_value('foreman', 'db_port')
-  db_config[:database] = param_value('foreman', 'db_database') || 'foreman'
-  db_config[:username] = param_value('foreman', 'db_username')
-  db_config[:password] = param_value('foreman', 'db_password')
-  db_config
-end
-
-def reset_database
-  Kafo::KafoConfigure.logger.info 'Dropping database!'
-
-  config = load_foreman_config
-  if remote_host?(config[:host])
-    empty_database!(config)
-  else
-    execute('DISABLE_DATABASE_ENVIRONMENT_CHECK=1 foreman-rake db:drop 2>&1')
-  end
-end
-
-def load_candlepin_config
-  db_config = {}
-  db_config[:host] = param_value('katello', 'candlepin_db_host') || 'localhost'
-  db_config[:port] = param_value('katello', 'candlepin_db_port')
-  db_config[:database] = param_value('katello', 'candlepin_db_name') || 'candlepin'
-  db_config[:username] = param_value('katello', 'candlepin_db_user')
-  db_config[:password] = param_value('katello', 'candlepin_db_password')
-  db_config
-end
-
-def empty_candlepin_database
-  config = load_candlepin_config
-  if remote_host?(config[:host])
-    empty_database!(config)
-  else
-    execute("sudo -u postgres dropdb #{config[:database]}")
-  end
-end
-
-def reset_candlepin
-  Kafo::KafoConfigure.logger.info 'Dropping Candlepin database!'
-  commands = [
-    'rm -f /var/lib/candlepin/cpdb_done',
-    'rm -f /var/lib/candlepin/cpinit_done',
-    'systemctl stop tomcat',
-  ]
-  execute(commands)
-  empty_candlepin_database
-end
-
-def empty_mongo
-  mongo_config = load_mongo_config
-  if remote_host?(mongo_config[:host])
-    empty_remote_mongo(mongo_config)
-  else
-    execute(
-      [
-        'systemctl start rh-mongodb34-mongod',
-        "mongo #{mongo_config[:database]} --eval 'db.dropDatabase();'",
-      ]
-    )
-  end
-end
-
-def load_mongo_config
-  config = {}
-  seeds = param_value('katello', 'pulp_db_seeds')
-  seed = seeds.split(',').first
-  host, port = seed.split(':') if seed
-  config[:host] = host || 'localhost'
-  config[:port] = port || '27017'
-  config[:database] = param_value('katello', 'pulp_db_name') || 'pulp_database'
-  config[:username] = param_value('katello', 'pulp_db_username')
-  config[:password] = param_value('katello', 'pulp_db_password')
-  config[:ssl] = param_value('katello', 'pulp_db_ssl') || false
-  config[:ca_path] = param_value('katello', 'pulp_db_ca_path')
-  config[:ssl_certfile] = param_value('katello', 'pulp_db_ssl_certfile')
-  config
-end
-
-def empty_remote_mongo(config)
-  if config[:ssl]
-    ssl = "--ssl"
-    if config[:ca_path]
-      ca_cert = "--sslCAFile #{config[:ca_path]}"
-      client_cert = "--sslPEMKeyFile #{config[:ssl_certfile]}" if config[:ssl_certfile]
-    end
-  end
-  username = "-u #{config[:username]}" if config[:username]
-  password = "-p #{config[:password]}" if config[:password]
-  host = "--host #{config[:host]} --port #{config[:port]}"
-  cmd = "mongo #{config[:database]} #{username} #{password} #{host} #{ssl} #{ca_cert} #{client_cert} --eval 'db.dropDatabase();'"
-  execute(cmd)
-end
-
-def reset_pulp
-  Kafo::KafoConfigure.logger.info 'Dropping Pulp database!'
-
-  execute(
-    [
-      'rm -f /var/lib/pulp/init.flag',
-      'systemctl stop httpd pulp_workers',
-    ]
-  )
-  empty_mongo
-  execute(
-    'rm -rf /var/lib/pulp/{distributions,published,repos}/*'
-  )
-end
-
-def load_pulpcore_config
-  db_config = {}
-  db_config[:host] = param_value('foreman_proxy_content', 'pulpcore_postgresql_host') || 'localhost'
-  db_config[:port] = param_value('foreman_proxy_content', 'pulpcore_postgresql_port') || 5432
-  db_config[:database] = param_value('foreman_proxy_content', 'pulpcore_postgresql_db_name') || 'pulpcore'
-  db_config[:username] = param_value('foreman_proxy_content', 'pulpcore_postgresql_user')
-  db_config[:password] = param_value('foreman_proxy_content', 'pulpcore_postgresql_password')
-  db_config
-end
-
-def reset_pulpcore
-  Kafo::KafoConfigure.logger.info 'Dropping Pulpcore database!'
-  config = load_pulpcore_config
-  if remote_host?(config[:host])
-    empty_database!(config)
-  else
-    execute("sudo -u postgres dropdb #{config[:database]}")
-  end
-end
-
-def remote_host?(hostname)
-  !['localhost', '127.0.0.1', `hostname`.strip].include?(hostname)
-end
-
-def pg_command_base(config, command, args)
-  port = "-p #{config[:port]}" if config[:port]
-  "PGPASSWORD='#{config[:password]}' #{command} -U #{config[:username]} -h #{config[:host]} #{port} #{args}"
-end
-
-def pg_sql_statement(config, statement)
-  pg_command_base(config, 'psql', "-d #{config[:database]} -t -c \"" + statement + '"')
-end
-
-# WARNING: deletes all the data from a database. No warnings. No confirmations.
-def empty_database!(config)
-  generate_delete_statements = pg_sql_statement(config, %q(
-        select string_agg('drop table if exists \"' || tablename || '\" cascade;', '')
-        from pg_tables
-        where schemaname = 'public';
-      ))
-  delete_statements = `#{generate_delete_statements}`
-  system(pg_sql_statement(config, delete_statements)) if delete_statements
-end
-
-if app_value(:reset) && !app_value(:noop)
-  response = ask('Are you sure you want to continue? This will drop the databases, reset all configurations that you have made and bring the server back to a fresh install. [y/n]')
-  if response.downcase != 'y'
-    $stderr.puts '** cancelled **'
-    kafo.class.exit(1)
-  else
-    reset
-  end
-end

commit a993d6bd6e42b0cc197d65f860b70ba307ecb29b

Fixes a bug in reset_candlepin introduced in 7ca7d88d

hooks/pre/10-reset_data.rb
@@ -53,7 +53,7 @@ end
 
 def reset_candlepin
   execute('rm -f /var/lib/candlepin/.puppet-candlepin-cpdb*')
-  empty_db_in_postgresql(db)
+  empty_db_in_postgresql('candlepin')
 end
 
 def empty_mongo