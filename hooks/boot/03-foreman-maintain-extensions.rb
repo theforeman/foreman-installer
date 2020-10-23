858e01ef97e22214ec67a8c8dae5074f720b75a6
Eric D. Helms <ericdhelms@gmail.com>
2020-10-23 00:25:27 +0000

    Fixes #31135: Introduce execute! and execute for fatal and non-fatal commands
    Introduce two separate functions that run a command. execute! will
    run a command and exit if the status indicates failure. execute will
    run a command and return the status of the command to let the hook
    take further action. execute is intended for actions that require
    information to decide what further action to take or if the action
    is non-fatal.
    
    This present previously seen errors where detecting foreman-maintain
    exists on the system or foreman-maintain checks for a feature
    from presenting as an error incorrectly to the user.
    
    (cherry picked from commit d5e079d0b4ccd6168a8f419bc002aac65b0607eb)

--- hooks/boot/03-foreman-maintain-extensions.rb
+++ hooks/boot/03-foreman-maintain-extensions.rb
@@ -1,13 +1,12 @@
 module ForemanMaintainHookContextExtension
   def foreman_maintain(command)
     command = "foreman-maintain #{command}"
-    execute_command(command, false, true)
+    execute(command, false, true)
   end
 
   def foreman_maintain!(command)
-    status = foreman_maintain(command)
-    exit 1 unless status
-    status
+    command = "foreman-maintain #{command}"
+    execute!(command, false, true)
   end
 end
 
--- hooks/pre/31-puppet_agent_oauth.rb
+++ hooks/pre/31-puppet_agent_oauth.rb
@@ -1,7 +1,9 @@
 unless app_value(:noop)
-  if File.exist?('/opt/puppetlabs/puppet/bin/ruby') && execute_command("rpm -q puppet-agent-oauth", false, false)
-    unless execute_command("/opt/puppetlabs/puppet/bin/ruby -e \"require 'oauth'\"", false, false)
-      execute_command("yum -y reinstall puppet-agent-oauth", false, true)
+  if File.exist?('/opt/puppetlabs/puppet/bin/ruby') && execute("rpm -q puppet-agent-oauth", false, false)
+    unless execute("/opt/puppetlabs/puppet/bin/ruby -e \"require 'oauth'\"", false, false)
+      success = execute("yum -y reinstall puppet-agent-oauth", false, true)
+
+      logger.error("Failed to reinstall puppet-agent-oauth. Please check that the package is available from a repository.") unless success
     end
   end
 end
