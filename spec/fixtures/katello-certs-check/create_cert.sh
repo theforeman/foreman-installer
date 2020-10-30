35ac9d2525ea8b9dc017af3f82df67ec80d8cdca
Eric D. Helms <ericdhelms@gmail.com>
2020-10-30 11:11:25 -0400

    Add subj-alt-name to certificate fixtures

--- spec/fixtures/katello-certs-check/create_cert.sh
+++ spec/fixtures/katello-certs-check/create_cert.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-# Generate CA
+echo "Generate CA"
 openssl genrsa -out ca.key 2048
-openssl req -x509 -new -nodes -key ca.key -sha256 -days 3650 -out cacert.crt
+openssl req -x509 -new -nodes -key ca.key -sha256 -days 3650 -out cacert.crt -subj "/CN=Test Self-Signed CA"
 
-# Generate server certificate
+echo "Generate server certificate"
 openssl genrsa -out foreman.example.com.key 2048
-openssl req -new -key foreman.example.com.key -out foreman.example.com.csr
+openssl req -new -key foreman.example.com.key -out foreman.example.com.csr -subj "/CN=foreman.example.com"
 openssl x509 -req -in foreman.example.com.csr -CA cacert.crt -CAkey ca.key -CAcreateserial -out foreman.example.com.crt -days 3650 -sha256 -extfile extensions.txt -extensions extensions
