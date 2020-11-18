5d7ed30f33afe0f1777549ee2938e32ca19c10b9
Eric D. Helms <ericdhelms@gmail.com>
2020-11-17 21:37:48 -0500

    Only regenerate certificate fixtures if they do not exist
    This allows adding more certificate fixtures without having to
    regenerate all certificates every time and leading to larger
    diffs.
    
    (cherry picked from commit 31b8240f205bdbaeca2ceeecaca690374b45d468)

--- spec/fixtures/katello-certs-check/README.md
+++ spec/fixtures/katello-certs-check/README.md
@@ -7,3 +7,5 @@ To generate a new set of certificates:
 ```
 
 In order to modify properties of the certificates, edit the `extensions.txt` before re-generating.
+
+To regenerate a particular set of certificates, remove them from the `certs/` directory and re-run the generation script.
--- spec/katello_certs_check_spec.rb
+++ spec/katello_certs_check_spec.rb
@@ -5,35 +5,39 @@ require 'open3'
 # badkey passphrase is 'foreman'
 
 describe 'katello-certs-check' do
-  let(:command) { File.join(__dir__, '..', 'bin', 'katello-certs-check') }
-  let(:directory) { File.join(FIXTURE_DIR, 'katello-certs-check') }
-  let(:ca) { File.join(directory, 'cacert.crt') }
-  let(:key) { File.join(directory, 'foreman.example.com.key') }
-  let(:cert) { File.join(directory, 'foreman.example.com.crt') }
-  let(:badkey) { File.join(directory, 'key_pass.key') }
-
   def fixture(filename)
     File.read(File.join(directory, filename)).gsub('|COMMAND|', command)
   end
 
-  it 'without parameters' do
-    stdout, stderr, status = Open3.capture3(command)
-    expect(stderr).to eq fixture('missing-parameter.txt')
-    expect(stdout).to eq ''
-    expect(status.exitstatus).to eq 1
-  end
+  let(:command) { File.join(__dir__, '..', 'bin', 'katello-certs-check') }
+  let(:directory) { File.join(FIXTURE_DIR, 'katello-certs-check') }
+  let(:certs_directory) { File.join(directory, 'certs') }
+  let(:ca) { File.join(certs_directory, 'ca.crt') }
 
-  it 'completes correctly' do
-    command_with_certs = "#{command} -b #{ca} -k #{key} -c #{cert}"
-    _stdout, stderr, status = Open3.capture3(command_with_certs)
-    expect(stderr).to eq ''
-    expect(status.exitstatus).to eq 0
-  end
+  context 'with valid certificates' do
+    let(:key) { File.join(certs_directory, 'foreman.example.com.key') }
+    let(:cert) { File.join(certs_directory, 'foreman.example.com.crt') }
+    let(:badkey) { File.join(directory, 'key_pass.key') }
 
-  it 'with password on key' do
-    command_with_certs = "#{command} -b #{ca} -k #{badkey} -c #{cert}"
-    _stdout, stderr, status = Open3.capture3(command_with_certs)
-    expect(stderr).to eq "The #{badkey} contains a passphrase, remove the key's passphrase by doing: \nmv #{badkey} #{badkey}.old \nopenssl rsa -in #{badkey}.old -out #{badkey}\n"
-    expect(status.exitstatus).to eq 1
+    it 'without parameters' do
+      stdout, stderr, status = Open3.capture3(command)
+      expect(stderr).to eq fixture('missing-parameter.txt')
+      expect(stdout).to eq ''
+      expect(status.exitstatus).to eq 1
+    end
+
+    it 'completes correctly' do
+      command_with_certs = "#{command} -b #{ca} -k #{key} -c #{cert}"
+      _stdout, stderr, status = Open3.capture3(command_with_certs)
+      expect(stderr).to eq ''
+      expect(status.exitstatus).to eq 0
+    end
+
+    it 'with password on key' do
+      command_with_certs = "#{command} -b #{ca} -k #{badkey} -c #{cert}"
+      _stdout, stderr, status = Open3.capture3(command_with_certs)
+      expect(stderr).to eq "The #{badkey} contains a passphrase, remove the key's passphrase by doing: \nmv #{badkey} #{badkey}.old \nopenssl rsa -in #{badkey}.old -out #{badkey}\n"
+      expect(status.exitstatus).to eq 1
+    end
   end
 end
