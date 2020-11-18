db5e641c743b05515735c228a6ffdf44ed232a0a
Eric D. Helms <ericdhelms@gmail.com>
2020-11-17 21:37:48 -0500

    Only regenerate certificate fixtures if they do not exist
    This allows adding more certificate fixtures without having to
    regenerate all certificates every time and leading to larger
    diffs.
    
    (cherry picked from commit 31b8240f205bdbaeca2ceeecaca690374b45d468)

--- spec/fixtures/katello-certs-check/create_cert.sh
+++ spec/fixtures/katello-certs-check/create_cert.sh
@@ -1,10 +1,22 @@
 #!/bin/bash
 
-echo "Generate CA"
-openssl genrsa -out ca.key 2048
-openssl req -x509 -new -nodes -key ca.key -sha256 -days 3650 -out cacert.crt -subj "/CN=Test Self-Signed CA"
+CERTS_DIR=certs
 
-echo "Generate server certificate"
-openssl genrsa -out foreman.example.com.key 2048
-openssl req -new -key foreman.example.com.key -out foreman.example.com.csr -subj "/CN=foreman.example.com"
-openssl x509 -req -in foreman.example.com.csr -CA cacert.crt -CAkey ca.key -CAcreateserial -out foreman.example.com.crt -days 3650 -sha256 -extfile extensions.txt -extensions extensions
+CA_CERT_NAME=ca
+if [[ ! -f "$CERTS_DIR/$CA_CERT_NAME.key" || ! -f "$CERTS_DIR/$CA_CERT_NAME.crt" ]]; then
+  echo "Generate CA"
+  openssl genrsa -out $CERTS_DIR/$CA_CERT_NAME.key 2048
+  openssl req -x509 -new -nodes -key $CERTS_DIR/$CA_CERT_NAME.key -sha256 -days 3650 -out $CERTS_DIR/$CA_CERT_NAME.crt -subj "/CN=Test Self-Signed CA"
+else
+  echo "CA certificate exists. Skipping."
+fi
+
+CERT_NAME=foreman.example.com
+if [[ ! -f "$CERTS_DIR/$CERT_NAME.key" || ! -f "$CERTS_DIR/$CERT_NAME.crt" ]]; then
+  echo "Generate server certificate"
+  openssl genrsa -out $CERTS_DIR/$CERT_NAME.key 2048
+  openssl req -new -key $CERTS_DIR/$CERT_NAME.key -out $CERTS_DIR/$CERT_NAME.csr -subj "/CN=foreman.example.com"
+  openssl x509 -req -in $CERTS_DIR/$CERT_NAME.csr -CA $CERTS_DIR/$CA_CERT_NAME.crt -CAkey $CERTS_DIR/$CA_CERT_NAME.key -CAcreateserial -out $CERTS_DIR/$CERT_NAME.crt -days 3650 -sha256 -extfile extensions.txt -extensions extensions
+else
+  echo "Server certificate exists. Skipping."
+fi
