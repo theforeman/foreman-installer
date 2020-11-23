55fcb2c0a88a8dd22090cc6b88003a48ea4aa67a
Jeremy Lenz <jlenz@redhat.com>
2020-11-23 11:18:55 -0500

    Refs #30436 - fix migration data shape
    Co-authored-by: William Bradford Clark <wclark@redhat.com>

--- config/katello.migrations/201117205851-add-import-export-params.rb
+++ config/katello.migrations/201117205851-add-import-export-params.rb
@@ -1,4 +1,4 @@
 if answers['foreman_proxy_content'].is_a?(Hash)
-  answers['foreman_proxy_content']['pulpcore']['allowed_import_path'] = ['/var/lib/pulp/sync_imports', '/var/lib/pulp/imports']
-  answers['foreman_proxy_content']['pulpcore']['allowed_export_path'] = ['/var/lib/pulp/exports']
+  answers['foreman_proxy_content']['pulpcore_allowed_import_path'] = ['/var/lib/pulp/sync_imports', '/var/lib/pulp/imports']
+  answers['foreman_proxy_content']['pulpcore_allowed_export_path'] = ['/var/lib/pulp/exports']
 end
