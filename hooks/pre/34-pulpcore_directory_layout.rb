79bf920c265ee9040e96a244542c826e26b2fd8f
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2020-12-23 16:37:29 +0100

    Fixes #31548 - Use the correct pulpcore legacy dir
    In our previous layout we used /var/lib/pulp/docroot as media directory
    while upstream used /var/lib/pulp as media directory and had all files
    in /var/lib/pulp/artifact. This artifact directory also exists in our
    previous layout, but that's /var/lib/pulp/docroot/artifact.
    
    Pulp can't move /var/lib/pulp to /var/lib/pulp/media so their migration
    was harder. We can just rename /var/lib/pulp/docroot to
    /var/lib/pulp/media and place a symlink in the old location.
    
    (cherry picked from commit 39003aaed97cb51285761463bdb84cf8bf0cdbca)

--- hooks/pre/34-pulpcore_directory_layout.rb
+++ hooks/pre/34-pulpcore_directory_layout.rb
@@ -1,14 +1,12 @@
 require 'pathname'
 
 PULP_ROOT = Pathname.new('/var/lib/pulp')
-LEGACY_DIR = PULP_ROOT / 'artifact'
-NEW_MEDIA_ROOT = PULP_ROOT / 'media'
-DESTINATION = NEW_MEDIA_ROOT / LEGACY_DIR.basename
+LEGACY_DIR = PULP_ROOT / 'docroot'
+DESTINATION = PULP_ROOT / 'media'
 
 if LEGACY_DIR.directory? && !LEGACY_DIR.symlink?
   logger.debug("Migrating #{LEGACY_DIR} to #{DESTINATION}")
   unless app_value(:noop)
-    NEW_MEDIA_ROOT.mkpath
     LEGACY_DIR.rename(DESTINATION)
     LEGACY_DIR.make_symlink(DESTINATION)
   end
