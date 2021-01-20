d1834ded3afec919a0311e2c2fc592f213394712
timdeluxe <tim.eilers@web.de>
2021-01-20 13:57:21 +0100

    Fixing puppet-agent-oauth check to also work with debian based systems

--- hooks/pre/31-puppet_agent_oauth.rb
+++ hooks/pre/31-puppet_agent_oauth.rb
@@ -6,4 +6,11 @@ unless app_value(:noop)
       logger.error("Failed to reinstall puppet-agent-oauth. Please check that the package is available from a repository.") unless success
     end
   end
+  if File.exist?('/opt/puppetlabs/puppet/bin/ruby') && execute("dpkg-query --show --showformat='${db:Status-Status}' puppet-agent-oauth | grep -q '^installed'", false, false)
+    unless execute("/opt/puppetlabs/puppet/bin/ruby -e \"require 'oauth'\"", false, false)
+      success = execute("apt-get --reinstall install puppet-agent-oauth", false, true)
+
+      logger.error("Failed to reinstall puppet-agent-oauth. Please check that the package is available from a repository.") unless success
+    end
+  end
 end
