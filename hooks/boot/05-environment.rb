1de17d5f0db52d557675242ae1359c39fe306fab
Eric D. Helms <ericdhelms@gmail.com>
2021-03-23 11:54:45 -0400

    Fixes #32180: Unset system proxy and SSL environment variables for the duration of the install
    Moves from checking for http proxy and SSL environment variables and
    failing if they are present to a model of unsetting them during
    the duration of the install run. This does not change them on
    the system itself.

---
+++ hooks/boot/05-environment.rb
@@ -0,0 +1,5 @@
+%w[http_proxy https_proxy ssl_cert_file HTTP_PROXY HTTPS_PROXY SSL_CERT_FILE].each do |variable|
+  if ::ENV.delete(variable)
+    logger.warn "Unsetting environment variable '#{variable}' for the duration of the install."
+  end
+end
