c0b38624a9f039fc2e7ba9d26753ed0d693a733a
Eric D. Helms <ericdhelms@gmail.com>
2021-05-18 08:52:22 -0400

    Fixes #32620: Unset NSS_DEFAULT_DB_TYPE during run
    When NSS_DEFAULT_DB_TYPE is set to, for example, sql then qpidd
    will fail to start.

--- hooks/boot/05-environment.rb
+++ hooks/boot/05-environment.rb
@@ -1,4 +1,4 @@
-%w[http_proxy https_proxy ssl_cert_file HTTP_PROXY HTTPS_PROXY SSL_CERT_FILE].each do |variable|
+%w[http_proxy https_proxy ssl_cert_file HTTP_PROXY HTTPS_PROXY SSL_CERT_FILE NSS_DEFAULT_DB_TYPE].each do |variable|
   if ::ENV.delete(variable)
     logger.warn "Unsetting environment variable '#{variable}' for the duration of the install."
   end
