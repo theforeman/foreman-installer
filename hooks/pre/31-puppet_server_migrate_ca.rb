10a3770af252847a2d28c7182eb45f5833a75a68
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2021-07-26 20:45:45 +0200

    Reflect changes for Puppetserver 7 support
    This adds a migration to clear --puppet-server-ssl-chain-filepath if
    it's the old default. It also stops the puppetserver service if needed
    to migrate the CA directory.

---
+++ hooks/pre/31-puppet_server_migrate_ca.rb
@@ -0,0 +1,16 @@
+# In Puppetserver 7 the ca directory has moved from
+# /etc/puppetlabs/puppet/ssl/ca to /etc/puppetlabs/puppetserver/ca
+# There is a command (puppetserver ca migrate) to migrate, but this requires
+# the server to be stopped. The Puppet module can execute the migration, but
+# can't stop the service.
+stdout_stderr, _status = execute_command('puppetserver --version', false, true)
+stdout_stderr&.match(/puppetserver version: (?<version>\d+)\.\d+\.\d+/) do |match|
+  old_ca_dir = '/etc/puppetlabs/puppet/ssl/ca'
+  if match[:version] == '7' && File.directory?(old_ca_dir) && !File.symlink?(old_ca_dir)
+    if app_value(:noop)
+      logger.debug 'Would stop puppetserver.service to migrate CA directory'
+    else
+      stop_services(['puppetserver.service'])
+    end
+  end
+end
