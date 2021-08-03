theforeman/foreman-installer

commit 567172b6411ed9d0731614fd29455e8b7877ccd0

Fixes #33191 - Debian puppet-agent-oauth reinstall support
This has been supported for a while on RPM-based distros. This adds it
on Debian.

It also changes it to log an error on noop that it detected an incorrect
situation.

Credits to @timdeluxe for the specific dpkg/apt commands. This PR takes
a bit different implementation approach by also looking at facts. This
should save some useless command executions on both platforms.

hooks/pre/31-puppet_agent_oauth.rb
@@ -1,9 +1,33 @@
-unless app_value(:noop)
-  if File.exist?('/opt/puppetlabs/puppet/bin/ruby') && execute("rpm -q puppet-agent-oauth", false, false)
-    unless execute("/opt/puppetlabs/puppet/bin/ruby -e \"require 'oauth'\"", false, false)
-      success = execute("yum -y reinstall puppet-agent-oauth", false, true)
+def oauth_aio_gem_installed?
+  return false unless File.exist?('/opt/puppetlabs/puppet/bin/ruby')
 
-      logger.error("Failed to reinstall puppet-agent-oauth. Please check that the package is available from a repository.") unless success
-    end
+  case facts['os']['family']
+  when 'Debian'
+    execute("dpkg-query --show --showformat='${db:Status-Status}' puppet-agent-oauth | grep -q '^installed'", false, false)
+  when 'RedHat'
+    execute("rpm -q puppet-agent-oauth", false, false)
+  end
+end
+
+def oauth_aio_gem_works?
+  execute("/opt/puppetlabs/puppet/bin/ruby -e \"require 'oauth'\"", false, false)
+end
+
+def reinstall_oauth_aio_gem
+  case facts['os']['family']
+  when 'Debian'
+    execute("apt-get --reinstall install puppet-agent-oauth", false, true)
+  when 'RedHat'
+    execute("yum -y reinstall puppet-agent-oauth", false, true)
+  end
+end
+
+if oauth_aio_gem_installed? && !oauth_aio_gem_works?
+  if app_value(:noop)
+    logger.error('Detected broken puppet-agent-oauth; needs reinstall')
+  else
+    success = reinstall_oauth_aio_gem
+
+    logger.error("Failed to reinstall puppet-agent-oauth. Please check that the package is available from a repository.") unless success
   end
 end

commit 82a4e9ecd93b32adc1384f3b4a19ded13e74e137

Refs #33191 - Use symbols for facts
In 567172b6411ed9d0731614fd29455e8b7877ccd0 facts were incorrectly using
strings while it should use symbols.

It also adds a debug line to make it easier to follow what happened.

Fixes: 567172b6411ed9d0731614fd29455e8b7877ccd0

hooks/pre/31-puppet_agent_oauth.rb
@@ -1,7 +1,7 @@
 def oauth_aio_gem_installed?
   return false unless File.exist?('/opt/puppetlabs/puppet/bin/ruby')
 
-  case facts['os']['family']
+  case facts[:os][:family]
   when 'Debian'
     execute("dpkg-query --show --showformat='${db:Status-Status}' puppet-agent-oauth | grep -q '^installed'", false, false)
   when 'RedHat'
@@ -14,7 +14,7 @@ def oauth_aio_gem_works?
 end
 
 def reinstall_oauth_aio_gem
-  case facts['os']['family']
+  case facts[:os][:family]
   when 'Debian'
     execute("apt-get --reinstall install puppet-agent-oauth", false, true)
   when 'RedHat'
@@ -26,6 +26,7 @@ if oauth_aio_gem_installed? && !oauth_aio_gem_works?
   if app_value(:noop)
     logger.error('Detected broken puppet-agent-oauth; needs reinstall')
   else
+    logger.debug('Detected broken puppet-agent-oauth; reinstalling')
     success = reinstall_oauth_aio_gem
 
     logger.error("Failed to reinstall puppet-agent-oauth. Please check that the package is available from a repository.") unless success