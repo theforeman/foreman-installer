071cd880312c53cad383004df50357be0fcf8679
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2021-08-26 21:50:30 +0200

    Stop trying to remove deleted parameters
    Kafo does this automatically and there's no need to write migrations for
    this. That was a misunderstanding on my side.

--- config/katello.migrations/190111180118-delete-removed-settings.rb
+++ config/katello.migrations/190111180118-delete-removed-settings.rb
@@ -1,44 +1,3 @@
-DELETED = {
-  'foreman' => [
-    'custom_repo',
-    'dynflow_in_core',
-    'email_conf',
-    'email_config_method',
-    'email_source',
-    'puppet_home',
-    'puppet_ssldir',
-  ],
-  'foreman_proxy' => [
-    'custom_repo',
-    'puppetca_modular',
-    'realm_split_config_files',
-    'use_autosignfile',
-  ],
-  'foreman_proxy::plugin::dhcp::infoblox' => [
-    'use_ranges',
-  ],
-  'puppet' => [
-    'agent_template',
-    'main_template',
-    'server_app_root',
-    'server_ca_proxy',
-    'server_directory_environments',
-    'server_dynamic_environments',
-    'server_environments',
-    'server_http_allow',
-    'server_httpd_service',
-    'server_implementation',
-    'server_main_template',
-    'server_passenger',
-    'server_passenger_min_instances',
-    'server_passenger_pre_start',
-    'server_passenger_ruby',
-    'server_rack_arguments',
-    'server_service_fallback',
-    'server_template',
-  ],
-}.freeze
-
 CLEAR = {
   'foreman' => [
     'authentication',
@@ -50,15 +9,6 @@ CLEAR = {
     'repo',
   ],
 }.freeze
-
-DELETED.each do |mod, parameters|
-  mod_answers = answers[mod]
-  next unless mod_answers.is_a?(Hash)
-
-  parameters.each do |parameter|
-    mod_answers.delete(parameter)
-  end
-end
 
 CLEAR.each do |mod, parameters|
   mod_answers = answers[mod]
