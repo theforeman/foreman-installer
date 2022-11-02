a2f06d43a461c34a59f81840c5955b490e146ac2
Leos Stejskal <lstejska@redhat.com>
2022-11-02 13:57:43 +0100

    Update config/foreman.migrations/20220406150414_foreman_google.rb
    Co-authored-by: Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>

--- config/foreman.migrations/20220406150414_foreman_google.rb
+++ config/foreman.migrations/20220406150414_foreman_google.rb
@@ -1,4 +1,4 @@
 old_answer = answers.delete('foreman::compute::gce')
 
 answers['foreman::plugin::google'] ||= old_answer || false
-answers['foreman::cli::google'] ||= old_answer || false
+answers['foreman::cli::google'] ||= answers['foreman::cli'] && answers['foreman::plugin::google']
