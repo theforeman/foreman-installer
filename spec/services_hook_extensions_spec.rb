31782b64031ad73afa5a0349d52891a633587594
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2023-07-06 16:06:01 +0200

    Make specs RuboCop clean

--- spec/services_hook_extensions_spec.rb
+++ spec/services_hook_extensions_spec.rb
@@ -3,8 +3,8 @@ require 'kafo/hook_context'
 require_relative '../hooks/boot/04-services'
 
 describe 'ForemanMaintainHookContextExtension' do
-  let(:kafo) { instance_double('KafoConfigure') }
-  let(:logger) { instance_double('Logger') }
+  let(:kafo) { instance_double(Kafo::KafoConfigure) }
+  let(:logger) { instance_double(Kafo::Logger) }
   let(:context) { Kafo::HookContext.new(kafo, logger) }
 
   before do
@@ -12,10 +12,10 @@ describe 'ForemanMaintainHookContextExtension' do
     allow(context).to receive(:fail_and_exit).and_raise(RuntimeError, 'called fail_and_exit')
   end
 
-  context 'start_services' do
+  describe '#start_services' do
     subject { context.start_services(services) }
 
-    context 'no services' do
+    context 'without services' do
       let(:services) { [] }
 
       it { expect { subject }.to raise_error(RuntimeError, 'Services must be specified') }
@@ -23,7 +23,7 @@ describe 'ForemanMaintainHookContextExtension' do
 
     context 'with services' do
       let(:output) { 'StdOut and StdErr' }
-      let(:status) { instance_double('Process::Status', exitstatus: exitstatus) }
+      let(:status) { instance_double(Process::Status, exitstatus: exitstatus) }
 
       before do
         allow(logger).to receive(:debug)
@@ -31,21 +31,21 @@ describe 'ForemanMaintainHookContextExtension' do
         allow(status).to receive(:success?).and_return(exitstatus == 0)
       end
 
-      context 'single service' do
+      context 'with a single service' do
         let(:services) { ['httpd'] }
 
-        context 'exit status 0' do
+        context 'with exit status 0' do
           let(:exitstatus) { 0 }
 
           it 'runs successfully' do
-            is_expected.to be_nil
+            expect(subject).to be_nil
 
             expect(logger).to have_received(:debug).with('Starting services httpd')
             expect(Open3).to have_received(:capture2e).with('systemctl', 'start', 'httpd')
           end
         end
 
-        context 'exit status 1' do
+        context 'with exit status 1' do
           let(:exitstatus) { 1 }
 
           before do
@@ -61,21 +61,21 @@ describe 'ForemanMaintainHookContextExtension' do
         end
       end
 
-      context 'multiple services' do
+      context 'with multiple services' do
         let(:services) { ['foreman', 'foreman-proxy'] }
 
-        context 'exit status 0' do
+        context 'with exit status 0' do
           let(:exitstatus) { 0 }
 
           it 'runs successfully' do
-            is_expected.to be_nil
+            expect(subject).to be_nil
 
             expect(logger).to have_received(:debug).with('Starting services foreman, foreman-proxy')
             expect(Open3).to have_received(:capture2e).with('systemctl', 'start', 'foreman', 'foreman-proxy')
           end
         end
 
-        context 'exit status 2' do
+        context 'with exit status 2' do
           let(:exitstatus) { 2 }
 
           before do
@@ -93,18 +93,18 @@ describe 'ForemanMaintainHookContextExtension' do
     end
   end
 
-  context 'stop_services' do
+  describe '#stop_services' do
     subject { context.stop_services(services) }
 
-    context 'no services' do
+    context 'without services' do
       let(:services) { [] }
 
       it { expect { subject }.to raise_error(RuntimeError, "Can't stop all services") }
     end
 
     context 'with service' do
-      let(:list_cmd_status) { instance_double('Process::Status', exitstatus: list_cmd_exitstatus) }
-      let(:stop_cmd_status) { instance_double('Process::Status', exitstatus: stop_cmd_exitstatus) }
+      let(:list_cmd_status) { instance_double(Process::Status, exitstatus: list_cmd_exitstatus) }
+      let(:stop_cmd_status) { instance_double(Process::Status, exitstatus: stop_cmd_exitstatus) }
 
       before do
         allow(logger).to receive(:debug)
@@ -112,14 +112,14 @@ describe 'ForemanMaintainHookContextExtension' do
         allow(list_cmd_status).to receive(:success?).and_return(list_cmd_exitstatus == 0)
       end
 
-      context 'httpd.service' do
+      context 'with httpd.service' do
         let(:services) { ['httpd.service'] }
 
-        context 'where the list command succeeds' do
+        context 'when the list command succeeds' do
           let(:list_cmd_stderr) { '' }
           let(:list_cmd_exitstatus) { 0 }
 
-          context 'returns no services' do
+          context 'when no services are returned' do
             before do
               allow(Open3).to receive(:capture2e).and_raise('Should not be called')
             end
@@ -127,7 +127,7 @@ describe 'ForemanMaintainHookContextExtension' do
             let(:list_cmd_stdout) { '' }
 
             it 'does not stop services' do
-              is_expected.to be_nil
+              expect(subject).to be_nil
 
               expect(logger).to have_received(:debug).with('Getting running services')
               expect(Open3).to have_received(:capture3).with('systemctl', 'list-units', '--no-legend', '--type=service,socket', '--state=running', 'httpd.service')
@@ -137,19 +137,20 @@ describe 'ForemanMaintainHookContextExtension' do
             end
           end
 
-          context 'returns the service' do
+          context 'when the service is returned' do
             let(:list_cmd_stdout) { "httpd.service loaded active running The Apache HTTP Server\n" }
+
             before do
               allow(Open3).to receive(:capture2e).and_return([stop_cmd_output, stop_cmd_status])
               allow(stop_cmd_status).to receive(:success?).and_return(stop_cmd_exitstatus == 0)
             end
 
-            context 'and stop command succeeds' do
+            context 'when the stop command succeeds' do
               let(:stop_cmd_output) { '' }
               let(:stop_cmd_exitstatus) { 0 }
 
-              it 'it runs successfully' do
-                is_expected.to be_nil
+              it 'runs successfully' do
+                expect(subject).to be_nil
 
                 expect(logger).to have_received(:debug).with('Getting running services')
                 expect(Open3).to have_received(:capture3).with('systemctl', 'list-units', '--no-legend', '--type=service,socket', '--state=running', 'httpd.service')
@@ -161,7 +162,7 @@ describe 'ForemanMaintainHookContextExtension' do
           end
         end
 
-        context 'where the list command fails' do
+        context 'when the list command fails' do
           let(:list_cmd_stdout) { '' }
           let(:list_cmd_stderr) { 'Failed StdErr' }
           let(:list_cmd_exitstatus) { 1 }
@@ -170,7 +171,7 @@ describe 'ForemanMaintainHookContextExtension' do
             allow(context).to receive(:exit)
           end
 
-          it 'it exits with an error' do
+          it 'exits with an error' do
             expect { subject }.to raise_error(RuntimeError, 'called fail_and_exit')
 
             expect(logger).to have_received(:debug).with('Getting running services')
