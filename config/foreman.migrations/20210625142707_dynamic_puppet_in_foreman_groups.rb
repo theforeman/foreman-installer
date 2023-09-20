e4902a90ae806fc9bf0e207641f28b0170defcf8
archanaserver <ak31960466@gmail.com>
2023-09-20 11:28:51 +0530

    Fix Rubocop Layout/IndentationWidth

--- config/foreman.migrations/20210625142707_dynamic_puppet_in_foreman_groups.rb
+++ config/foreman.migrations/20210625142707_dynamic_puppet_in_foreman_groups.rb
@@ -1,3 +1,3 @@
 if answers['foreman'].is_a?(Hash) && (answers['foreman']['user_groups'] && answers['foreman']['user_groups'].include?('puppet'))
-    answers['foreman']['user_groups'].delete('puppet')
+  answers['foreman']['user_groups'].delete('puppet')
 end
--- config/katello.migrations/210819164117-reset-puma-workers-and-max-threads.rb
+++ config/katello.migrations/210819164117-reset-puma-workers-and-max-threads.rb
@@ -1,8 +1,8 @@
 # https://github.com/theforeman/puppet-foreman/commit/533c1f3be8069139d7375841019afeb7c1102830
 if answers['foreman'].is_a?(Hash) && (answers['foreman']['foreman_service_puma_workers'] == 2)
-    answers['foreman'].delete('foreman_service_puma_workers')
+  answers['foreman'].delete('foreman_service_puma_workers')
 end
 
 if answers['foreman'].is_a?(Hash) && (answers['foreman']['foreman_service_puma_threads_max'] == 16)
-    answers['foreman'].delete('foreman_service_puma_threads_max')
+  answers['foreman'].delete('foreman_service_puma_threads_max')
 end
