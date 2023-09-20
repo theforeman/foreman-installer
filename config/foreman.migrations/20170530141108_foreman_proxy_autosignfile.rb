0b6f9011181cffbb12626f5d8e22bf313e5d8c5b
archanaserver <ak31960466@gmail.com>
2023-09-20 11:23:27 +0530

    Fix Rubocop Style/StringConcatenation

--- config/foreman.migrations/20170530141108_foreman_proxy_autosignfile.rb
+++ config/foreman.migrations/20170530141108_foreman_proxy_autosignfile.rb
@@ -2,6 +2,6 @@ if answers['foreman_proxy'] && !answers['foreman_proxy'].has_key?('use_autosignf
   answers['foreman_proxy']['use_autosignfile'] = true
   if answers['foreman_proxy'].has_key?('puppetdir')
     puppetdir = answers['foreman_proxy']['puppetdir']
-    answers['foreman_proxy']['autosignfile'] = puppetdir + '/autosign.conf'
+    answers['foreman_proxy']['autosignfile'] = "#{puppetdir}/autosign.conf"
   end
 end
