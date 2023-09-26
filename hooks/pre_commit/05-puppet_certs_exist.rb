482f3b4d814a2075cbefd402f2dd253383016338
archanaserver <ak31960466@gmail.com>
2023-09-26 14:43:04 +0530

    Fix Rubocop Layout/LineContinuationLeadingSpace

--- hooks/pre_commit/05-puppet_certs_exist.rb
+++ hooks/pre_commit/05-puppet_certs_exist.rb
@@ -8,10 +8,10 @@ client_message = "- is Puppet already installed without Puppet CA? You can remov
 
 if puppet_ca_enabled && key_path && File.exist?(key_path) && !kafo.skip_checks_i_know_better?
   if !puppet_ca_cert.empty? && !File.exist?(puppet_ca_cert)
-    fail_and_exit("The file #{puppet_ca_cert} does not exist.\n #{client_message}\n" \
-    " - if you use custom Puppet SSL directory (--foreman-proxy-ssldir) make sure the directory exists and contain the CA certificate.\n", 101)
+    fail_and_exit("The file #{puppet_ca_cert} does not exist.\n #{client_message}\n " \
+                  "- if you use custom Puppet SSL directory (--foreman-proxy-ssldir) make sure the directory exists and contain the CA certificate.\n", 101)
   end
   if server_crl_path.is_a?(String) && !server_crl_path.empty? && !File.exist?(server_crl_path)
-    fail_and_exit("The file #{server_crl_path} does not exist.\n #{client_message}\n - if you set custom revocation list (--foreman-server-ssl-crl) make sure the file exists.", 101)
+    fail_and_exit("The file #{server_crl_path} does not exist.\n#{client_message}\n - if you set a custom revocation list (--foreman-server-ssl-crl) make sure the file exists.", 101)
   end
 end
