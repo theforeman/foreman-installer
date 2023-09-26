c300a0cc180fe1973ae1b7eb6bec2c695d4addb1
archanaserver <ak31960466@gmail.com>
2023-09-26 14:58:07 +0530

    Fix Rubocop Layout/LineEndStringConcatenationIndentation

--- hooks/boot/09-version_locking.rb
+++ hooks/boot/09-version_locking.rb
@@ -7,8 +7,8 @@ if package_lock_feature?
     '--[no-]lock-package-versions',
     :flag,
     "Let installer lock versions of the installed packages to prevent\n" \
-      "unexpected breakages during system updates. The choice is remembered\n" \
-      "and used in next installer runs.",
+    "unexpected breakages during system updates. The choice is remembered\n" \
+    "and used in next installer runs.",
     :default => nil
   )
 end
