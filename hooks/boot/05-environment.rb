02c019b4c2a80791f3b0becb77c27efcea87ff17
archanaserver <ak31960466@gmail.com>
2023-10-03 18:02:19 +0530

    Fix Rubocop Style/RedundantConstantBase

--- hooks/boot/05-environment.rb
+++ hooks/boot/05-environment.rb
@@ -1,5 +1,5 @@
 %w[http_proxy https_proxy ssl_cert_file HTTP_PROXY HTTPS_PROXY SSL_CERT_FILE].each do |variable|
-  if ::ENV.delete(variable)
+  if ::ENV.delete(variable) # rubocop:disable Style/RedundantConstantBase
     logger.warn "Unsetting environment variable '#{variable}' for the duration of the install."
   end
 end
