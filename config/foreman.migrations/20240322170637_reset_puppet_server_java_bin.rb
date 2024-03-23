322bebb07e2728bda786cf5eaf6ca69e94711cba
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2024-03-23 12:39:24 +0100

    Refs #37291 - Only reset puppetserver's JVM if it's /usr/bin/java
    The intent was to only reset it if it was the default value, not just
    any value.
    
    Fixes: 19310592c8d7f16b2c59f0995463e7ae913e01ef ("Refs #37291 - Reset Puppet's java_bin")

--- config/foreman.migrations/20240322170637_reset_puppet_server_java_bin.rb
+++ config/foreman.migrations/20240322170637_reset_puppet_server_java_bin.rb
@@ -1,3 +1,3 @@
-if answers['puppet'].is_a?(Hash) && answers['puppet']['server_jvm_java_bin']
+if answers['puppet'].is_a?(Hash) && answers['puppet']['server_jvm_java_bin'] == '/usr/bin/java'
   answers['puppet'].delete('server_jvm_java_bin')
 end
