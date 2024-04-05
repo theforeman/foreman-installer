b4fda4bfa3eecd31dc801ff2781011e00fa64bca
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2024-04-05 21:28:27 +0200

    Factor out FakePuppetfile so it can be tested in rspec

---
+++ spec/util/fake_puppet_file_spec.rb
@@ -0,0 +1,33 @@
+require 'spec_helper'
+
+require_relative '../../util/fake_puppet_file'
+
+describe FakePuppetfile do
+  let(:fake) { FakePuppetfile.new }
+
+  specify 'without a version' do
+    expect(PuppetForge::Module).not_to receive(:find)
+
+    fake.instance_eval { mod 'theforeman/motd' }
+
+    expect { |b| fake.content(&b) }.to yield_with_args("mod 'theforeman/motd'")
+  end
+
+  specify 'with a minimum' do
+    expect(PuppetForge::Module).not_to receive(:find)
+
+    fake.instance_eval { mod 'theforeman/motd', '>= 1.2' }
+
+    expect { |b| fake.content(&b) }.to yield_with_args("mod 'theforeman/motd', '>= 1.2'")
+  end
+
+  specify 'with a git url' do
+    expect(PuppetForge::Module).to receive(:find)
+      .with('theforeman-motd')
+      .and_return(double(PuppetForge::V3::Module, current_release: double(PuppetForge::V3::Release, version: '1.2.3')))
+
+    fake.instance_eval { mod 'theforeman/motd', :git => 'https://github.com/theforeman/puppet-motd' }
+
+    expect { |b| fake.content(&b) }.to yield_with_args("mod 'theforeman/motd', '~> 1.2.3'")
+  end
+end
