theforeman/foreman-installer

commit 24a81c701f3774cb55e595e6dd773309e09ae6ca

Factor out FakePuppetfile so it can be tested in rspec

Rakefile
@@ -15,56 +15,6 @@ rescue LoadError
   puts 'Rubocop not loaded'
 end
 
-begin
-  require 'puppet_forge'
-  require 'semverse'
-
-  class FakePuppetfile
-    def initialize
-      @new_content = []
-    end
-
-    def forge(url)
-      @new_content << ['forge', url, nil]
-      PuppetForge.host = url
-    end
-
-    def mod(name, options = nil)
-      if options.is_a?(Hash) && !options.include?(:ref)
-        release = PuppetForge::Module.find(name.tr('/', '-')).current_release
-        @new_content << ['mod', name, "~> #{release.version}"]
-      else
-        @new_content << ['mod', name, options]
-      end
-    end
-
-    def content
-      max_length = @new_content.select { |type, _value| type == 'mod' }.map { |_type, value| value.length }.max
-
-      @new_content.each do |type, value, options|
-        if type == 'forge'
-          yield "forge '#{value}'"
-          yield ""
-        elsif type == 'mod'
-          if options.nil?
-            yield "mod '#{value}'"
-          elsif options.is_a?(String)
-            padding = ' ' * (max_length - value.length)
-            yield "mod '#{value}', #{padding}'#{options}'"
-          else
-            padding = ' ' * (max_length - value.length)
-            yield "mod '#{value}', #{padding}#{options.map { |k, v| ":#{k} => '#{v}'" }.join(', ')}"
-          end
-        end
-      end
-    end
-  end
-
-  pin_task = true
-rescue LoadError
-  pin_task = false
-end
-
 BUILD_KATELLO = !ENV.key?('EXCLUDE_KATELLO')
 
 BUILDDIR = File.expand_path(ENV['BUILDDIR'] || '_build')
@@ -363,13 +313,17 @@ namespace :pkg do
   end
 end
 
-if pin_task
+begin
+  require_relative 'util/fake_puppet_file'
+rescue LoadError
+  # Some dependency missing
+else
   desc 'Pin all the modules in Puppetfile to released versions instead of git branches'
   task :pin_modules do
     filename = 'Puppetfile'
 
     fake = FakePuppetfile.new
-    fake.instance_eval { instance_eval(File.read(filename), filename, 1) }
+    fake.instance_eval { eval(File.read(filename), filename, 1) }
 
     File.open(filename, 'w') do |file|
       fake.content do |line|

spec/util/fake_puppet_file_spec.rb
@@ -1,0 +1,33 @@
+require 'spec_helper'
+
+require_relative '../../util/fake_puppet_file'
+
+describe FakePuppetfile do
+  let(:fake) { FakePuppetfile.new }
+
+  specify 'without a version' do
+    expect(PuppetForge::Module).not_to receive(:find)
+
+    fake.instance_eval { mod 'theforeman/motd' }
+
+    expect { |b| fake.content(&b) }.to yield_with_args("mod 'theforeman/motd'")
+  end
+
+  specify 'with a minimum' do
+    expect(PuppetForge::Module).not_to receive(:find)
+
+    fake.instance_eval { mod 'theforeman/motd', '>= 1.2' }
+
+    expect { |b| fake.content(&b) }.to yield_with_args("mod 'theforeman/motd', '>= 1.2'")
+  end
+
+  specify 'with a git url' do
+    expect(PuppetForge::Module).to receive(:find)
+      .with('theforeman-motd')
+      .and_return(double(PuppetForge::V3::Module, current_release: double(PuppetForge::V3::Release, version: '1.2.3')))
+
+    fake.instance_eval { mod 'theforeman/motd', :git => 'https://github.com/theforeman/puppet-motd' }
+
+    expect { |b| fake.content(&b) }.to yield_with_args("mod 'theforeman/motd', '~> 1.2.3'")
+  end
+end

util/fake_puppet_file.rb
@@ -1,0 +1,43 @@
+require 'puppet_forge'
+require 'semverse'
+
+class FakePuppetfile
+  def initialize
+    @new_content = []
+  end
+
+  def forge(url)
+    @new_content << ['forge', url, nil]
+    PuppetForge.host = url
+  end
+
+  def mod(name, options = nil)
+    if options.is_a?(Hash) && !options.include?(:ref)
+      release = PuppetForge::Module.find(name.tr('/', '-')).current_release
+      @new_content << ['mod', name, "~> #{release.version}"]
+    else
+      @new_content << ['mod', name, options]
+    end
+  end
+
+  def content
+    max_length = @new_content.select { |type, _value| type == 'mod' }.map { |_type, value| value.length }.max
+
+    @new_content.each do |type, value, options|
+      if type == 'forge'
+        yield "forge '#{value}'"
+        yield ""
+      elsif type == 'mod'
+        if options.nil?
+          yield "mod '#{value}'"
+        elsif options.is_a?(String)
+          padding = ' ' * (max_length - value.length)
+          yield "mod '#{value}', #{padding}'#{options}'"
+        else
+          padding = ' ' * (max_length - value.length)
+          yield "mod '#{value}', #{padding}#{options.map { |k, v| ":#{k} => '#{v}'" }.join(', ')}"
+        end
+      end
+    end
+  end
+end

commit 951a2be4126a62f3af8cf162c65e3a5103ff8943

Correctly evaluate the Puppetfile in pin_modules
For some reason I changed it to eval instead of instance_eval and this
broke.

Fixes: 24a81c701f37 ("Factor out FakePuppetfile so it can be tested in rspec")

Rakefile
@@ -323,7 +323,7 @@ else
     filename = 'Puppetfile'
 
     fake = FakePuppetfile.new
-    fake.instance_eval { eval(File.read(filename), filename, 1) }
+    fake.instance_eval { instance_eval(File.read(filename), filename, 1) }
 
     File.open(filename, 'w') do |file|
       fake.content do |line|