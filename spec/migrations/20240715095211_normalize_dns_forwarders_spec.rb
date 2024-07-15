cabd312b7fc580186a7347b5f899c6520e6dbb84
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2024-07-15 12:06:19 +0200

    Refs #37604 - Normalize DNS forwarders to an array
    Previously the documentation showed an example that abused an internal
    implementation detail that's no longer allowed with the stricter input
    validation.

---
+++ spec/migrations/20240715095211_normalize_dns_forwarders_spec.rb
@@ -0,0 +1,33 @@
+require 'spec_helper'
+
+migration '20240715095211_normalize_dns_forwarders' do
+  scenarios %w[foreman katello foreman-proxy-content] do
+    context 'valid array' do
+      let(:answers) do
+        {
+          'foreman_proxy' => {
+            'forwarders' => ['192.0.2.1', '192.0.2.2'],
+          },
+        }
+      end
+
+      it 'leaves the answers untouched' do
+        expect(migrated_answers['foreman_proxy']['forwarders']).to eq(['192.0.2.1', '192.0.2.2'])
+      end
+    end
+
+    context 'semicolon separated value' do
+      let(:answers) do
+        {
+          'foreman_proxy' => {
+            'forwarders' => ['192.0.2.1; 192.0.2.2'],
+          },
+        }
+      end
+
+      it 'leaves the answers untouched' do
+        expect(migrated_answers['foreman_proxy']['forwarders']).to eq(['192.0.2.1', '192.0.2.2'])
+      end
+    end
+  end
+end
