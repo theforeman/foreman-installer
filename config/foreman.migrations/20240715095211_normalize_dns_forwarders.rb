e4fc6826b87177e7629258ea8b0499ebc0b559ea
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2024-07-15 12:06:19 +0200

    Refs #37604 - Normalize DNS forwarders to an array
    Previously the documentation showed an example that abused an internal
    implementation detail that's no longer allowed with the stricter input
    validation.

---
+++ config/foreman.migrations/20240715095211_normalize_dns_forwarders.rb
@@ -0,0 +1,8 @@
+# forwarders was always an array, but previously it was documented as
+# --foreman-proxy-dns-forwarders "192.0.2.1; 192.0.2.2"
+fp_mod = answers['foreman_proxy']
+if fp_mod.is_a?(Hash) && fp_mod['forwarders']
+  fp_mod['forwarders'] = fp_mod['forwarders'].flat_map do |forwarder|
+    forwarder.split(';').map(&:strip)
+  end
+end
