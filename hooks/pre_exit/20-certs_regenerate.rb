147f8376b9851f02d15cc3275f65e388a5f2ce5d
Eric Helms <eric.d.helms@gmail.com>
2024-07-30 15:43:47 -0400

    Update hooks/pre_exit/20-certs_regenerate.rb
    Co-authored-by: Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>

--- hooks/pre_exit/20-certs_regenerate.rb
+++ hooks/pre_exit/20-certs_regenerate.rb
@@ -1,4 +1,4 @@
-if module_enabled?('certs') && param('certs', 'regenerate').value == true
+if !app_value(:noop) && !app_value(:dont_save_answers) && module_enabled?('certs') && param('certs', 'regenerate').value == true
   kafo.config.modules.each do |mod|
     if mod.identifier == 'certs'
       mod.params_hash['regenerate'] = false
