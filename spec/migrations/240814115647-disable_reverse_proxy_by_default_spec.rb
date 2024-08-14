471f9ff9a04427c7b8b5a07f566ce511eba95678
Eric D. Helms <ericdhelms@gmail.com>
2024-08-14 08:03:06 -0400

    Disable reverse proxy by default

---
+++ spec/migrations/240814115647-disable_reverse_proxy_by_default_spec.rb
@@ -0,0 +1,19 @@
+require 'spec_helper'
+
+migration '240814115647-disable-reverse-proxy-by-default' do
+  scenarios %w[foreman-proxy-content] do
+    context 'reset reverse_proxy to default value to disable by default' do
+      let(:answers) do
+        {
+          'foreman_proxy_content' => {
+            'reverse_proxy' => true,
+          },
+        }
+      end
+
+      it 'changes the default to the puppet module' do
+        expect(migrated_answers['foreman_proxy_content']['reverse_proxy']).to eq(nil)
+      end
+    end
+  end
+end
