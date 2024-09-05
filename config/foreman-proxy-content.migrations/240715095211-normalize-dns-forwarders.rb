theforeman/foreman-installer

commit e4fc6826b87177e7629258ea8b0499ebc0b559ea

Refs #37604 - Normalize DNS forwarders to an array
Previously the documentation showed an example that abused an internal
implementation detail that's no longer allowed with the stricter input
validation.

config/foreman-proxy-content.migrations/240715095211-normalize-dns-forwarders.rb
@@ -1,0 +1,8 @@
+# forwarders was always an array, but previously it was documented as
+# --foreman-proxy-dns-forwarders "192.0.2.1; 192.0.2.2"
+fp_mod = answers['foreman_proxy']
+if fp_mod.is_a?(Hash) && fp_mod['forwarders']
+  fp_mod['forwarders'] = fp_mod['forwarders'].flat_map do |forwarder|
+    forwarder.split(';').map(&:strip)
+  end
+end

config/foreman.migrations/20240715095211_normalize_dns_forwarders.rb
@@ -1,0 +1,8 @@
+# forwarders was always an array, but previously it was documented as
+# --foreman-proxy-dns-forwarders "192.0.2.1; 192.0.2.2"
+fp_mod = answers['foreman_proxy']
+if fp_mod.is_a?(Hash) && fp_mod['forwarders']
+  fp_mod['forwarders'] = fp_mod['forwarders'].flat_map do |forwarder|
+    forwarder.split(';').map(&:strip)
+  end
+end

config/katello.migrations/240715095211-normalize-dns-forwarders.rb
@@ -1,0 +1,8 @@
+# forwarders was always an array, but previously it was documented as
+# --foreman-proxy-dns-forwarders "192.0.2.1; 192.0.2.2"
+fp_mod = answers['foreman_proxy']
+if fp_mod.is_a?(Hash) && fp_mod['forwarders']
+  fp_mod['forwarders'] = fp_mod['forwarders'].flat_map do |forwarder|
+    forwarder.split(';').map(&:strip)
+  end
+end

spec/migrations/20240715095211_normalize_dns_forwarders_spec.rb
@@ -1,0 +1,33 @@
+require 'spec_helper'
+
+migration '20240715095211_normalize_dns_forwarders' do
+  scenarios %w[foreman katello foreman-proxy-content] do
+    context 'valid array' do
+      let(:answers) do
+        {
+          'foreman_proxy' => {
+            'forwarders' => ['192.0.2.1', '192.0.2.2'],
+          },
+        }
+      end
+
+      it 'leaves the answers untouched' do
+        expect(migrated_answers['foreman_proxy']['forwarders']).to eq(['192.0.2.1', '192.0.2.2'])
+      end
+    end
+
+    context 'semicolon separated value' do
+      let(:answers) do
+        {
+          'foreman_proxy' => {
+            'forwarders' => ['192.0.2.1; 192.0.2.2'],
+          },
+        }
+      end
+
+      it 'leaves the answers untouched' do
+        expect(migrated_answers['foreman_proxy']['forwarders']).to eq(['192.0.2.1', '192.0.2.2'])
+      end
+    end
+  end
+end

commit 159e17a8f7e9b6782d3c610dbaa7017dc9b2eeb3

Refs #37604 - Use the correct answer in DNS normalize migration
The previous patch used forwarders instead of dns_forwarders.

Fixes: e4fc6826b871 ("Refs #37604 - Normalize DNS forwarders to an array")

config/foreman-proxy-content.migrations/240715095211-normalize-dns-forwarders.rb
@@ -1,8 +1,8 @@
 # forwarders was always an array, but previously it was documented as
 # --foreman-proxy-dns-forwarders "192.0.2.1; 192.0.2.2"
 fp_mod = answers['foreman_proxy']
-if fp_mod.is_a?(Hash) && fp_mod['forwarders']
-  fp_mod['forwarders'] = fp_mod['forwarders'].flat_map do |forwarder|
+if fp_mod.is_a?(Hash) && fp_mod['dns_forwarders']
+  fp_mod['dns_forwarders'] = fp_mod['dns_forwarders'].flat_map do |forwarder|
     forwarder.split(';').map(&:strip)
   end
 end

config/foreman.migrations/20240715095211_normalize_dns_forwarders.rb
@@ -1,8 +1,8 @@
 # forwarders was always an array, but previously it was documented as
 # --foreman-proxy-dns-forwarders "192.0.2.1; 192.0.2.2"
 fp_mod = answers['foreman_proxy']
-if fp_mod.is_a?(Hash) && fp_mod['forwarders']
-  fp_mod['forwarders'] = fp_mod['forwarders'].flat_map do |forwarder|
+if fp_mod.is_a?(Hash) && fp_mod['dns_forwarders']
+  fp_mod['dns_forwarders'] = fp_mod['dns_forwarders'].flat_map do |forwarder|
     forwarder.split(';').map(&:strip)
   end
 end

config/katello.migrations/240715095211-normalize-dns-forwarders.rb
@@ -1,8 +1,8 @@
 # forwarders was always an array, but previously it was documented as
 # --foreman-proxy-dns-forwarders "192.0.2.1; 192.0.2.2"
 fp_mod = answers['foreman_proxy']
-if fp_mod.is_a?(Hash) && fp_mod['forwarders']
-  fp_mod['forwarders'] = fp_mod['forwarders'].flat_map do |forwarder|
+if fp_mod.is_a?(Hash) && fp_mod['dns_forwarders']
+  fp_mod['dns_forwarders'] = fp_mod['dns_forwarders'].flat_map do |forwarder|
     forwarder.split(';').map(&:strip)
   end
 end

spec/migrations/20240715095211_normalize_dns_forwarders_spec.rb
@@ -6,13 +6,13 @@ migration '20240715095211_normalize_dns_forwarders' do
       let(:answers) do
         {
           'foreman_proxy' => {
-            'forwarders' => ['192.0.2.1', '192.0.2.2'],
+            'dns_forwarders' => ['192.0.2.1', '192.0.2.2'],
           },
         }
       end
 
       it 'leaves the answers untouched' do
-        expect(migrated_answers['foreman_proxy']['forwarders']).to eq(['192.0.2.1', '192.0.2.2'])
+        expect(migrated_answers['foreman_proxy']['dns_forwarders']).to eq(['192.0.2.1', '192.0.2.2'])
       end
     end
 
@@ -20,13 +20,13 @@ migration '20240715095211_normalize_dns_forwarders' do
       let(:answers) do
         {
           'foreman_proxy' => {
-            'forwarders' => ['192.0.2.1; 192.0.2.2'],
+            'dns_forwarders' => ['192.0.2.1; 192.0.2.2'],
           },
         }
       end
 
-      it 'leaves the answers untouched' do
-        expect(migrated_answers['foreman_proxy']['forwarders']).to eq(['192.0.2.1', '192.0.2.2'])
+      it 'normalizes the answer' do
+        expect(migrated_answers['foreman_proxy']['dns_forwarders']).to eq(['192.0.2.1', '192.0.2.2'])
       end
     end
   end