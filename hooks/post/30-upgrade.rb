2d55a704788a0367d2eda1d29aaf70f1cd03e685
Tim Meusel <tim@bastelfreak.de>
2025-10-17 15:34:07 +0200

    rubocop: Update 0.80->1.81

--- hooks/post/30-upgrade.rb
+++ hooks/post/30-upgrade.rb
@@ -1,6 +1,6 @@
 def run_rake_upgrade?
   it = get_custom_config(:run_rake_upgrade)
-  it.nil? ? true : it
+  it.nil? || it
 end
 
 if !app_value(:noop) && [0, 2].include?(@kafo.exit_code) && foreman_server? && run_rake_upgrade?
