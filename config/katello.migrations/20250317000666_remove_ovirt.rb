8c4ed9671732e6311a65f9797755f3729904300f
Nadja Heitmann <nadjah@atix.de>
2025-11-05 13:50:14 +0100

    Refs #38921 - Install foreman_ovirt plugin if oVirt was enabled before
    (cherry picked from commit 17a38cba92ac162c860df176d372ee5f0ee57f14)

--- config/katello.migrations/20250317000666_remove_ovirt.rb
+++ config/katello.migrations/20250317000666_remove_ovirt.rb
@@ -1,2 +1,5 @@
-answers.delete('foreman::compute::ovirt')
+# install foreman_ovirt plugin if oVirt compute resource was enabled before
+compute_ovirt = answers.delete('foreman::compute::ovirt')
+answers['foreman::plugin::ovirt'] = compute_ovirt.is_a?(Hash) || compute_ovirt
+
 answers.delete('foreman::plugin::ovirt_provision')
